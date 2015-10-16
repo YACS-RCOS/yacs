97afedadcf79076d36f7b3f9c2229c14afad6184
Richie Young <richie@Richies-MacBook-Pro.local>
2015-10-15 21:43:24 -0400

    adds default test department factory

--- spec/factories/departments.rb
+++ spec/factories/departments.rb
@@ -1,4 +1,8 @@
 FactoryGirl.define do
+  factory :department do
+    code 'TEST'
+    name 'Test Department'
+  end
   factory :csci_department, class: Department do
     code 'CSCI'
     name 'Computer Science'
