29c93d792c0f699866b4579d8242ccc3053afa37
Richie Young <richie@Richies-MacBook-Pro.local>
2015-12-08 23:37:04 -0500

    [reset database please] removes some migrations for eliminated tables

--- db/migrate/20151008012040_create_professors.rb
+++
@@ -1,9 +0,0 @@
-class CreateProfessors < ActiveRecord::Migration
-  def change
-    create_table :professors do |t|
-      t.string :name      ,null:false
-      t.timestamps         null:false
-    end
-    add_index :professors, :name # speed up search by professor name
-  end
-end
