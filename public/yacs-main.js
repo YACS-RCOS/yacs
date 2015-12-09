77f0c058d8db0c7dd2b6e5d689a65dad0d4e6ef0
aosdict <aosdict@gmail.com>
2015-12-09 15:32:01 -0500

    Implemented multiple columns in the home page

--- public/yacs-main.js
+++ public/yacs-main.js
@@ -1,6 +1,12 @@
 // yacs namespace
 var nsYacs = {}
 
+// Global constants are all set here to keep them in one place.
+nsYacs.deptColumnWidth = 600; // should be the same as the width
+                              // defined for <school> in yacs-main.css
+nsYacs.deptColumnMargin = 10; // should be the same as side margins defined
+                              // for <school> in yacs-main.css
+
 /* Format some items which appear on the search results page into their final
    display. The API does not load text like "credits" or "Section"; the
    application is responsible for this process.
@@ -11,25 +17,156 @@ function formatSearchResults() {
     else { return oldhtml + ' credits'; }
   });
   $('section name').before('Section ');
-  $('seats')
+  //$('seats').
 }
 
-/* Given a filename which is a public XML document on the server,
-   replace the interior of div#content with it. Styling should be done
-   automatically.
+/* Given a filename which is a public XML document on the server, return the
+   content of that document.
    This will work with API documents, which should be on an root-relative path
    like "/api/v5/whatever.xml"
 */
-function replaceContent(filename) {
+function getAPIContent(filename) {
   var request = new XMLHttpRequest();
+  var ret = '';
   request.addEventListener("load", function() {
-    nsYacs.contentContainer.innerHTML = request.response;
+    ret = request.response;
   });
   // I don't see a problem with forcing this request to be synchronous, because
   // it's loading the entire page content. Other things like formatting that
   // need to wait on this to work properly _should_ wait on it.
+  // This is known to raise a jQuery warning that this (synchronous)
+  // functionality is deprecated.
   request.open("GET", filename, false); 
   request.send();
+  return ret;
+}
+
+// Given a filename which is a public XML document on the server,
+// replace the interior of div#content with it
+function replaceContent(filename) {
+  nsYacs.contentContainer.innerHTML = getAPIContent(filename);
+}
+
+// Anything that has to be done when loading up the front page.
+function loadHomePage() {
+  replaceContent("/api/v5/departments.xml?use_schools=false");
+
+  // When loading the home page, this must determine how many columns to
+  // place the departments in, and then apply styling as needed to make them fit
+  // into that many columns. Our approach is to always use the maximum possible
+  // number of columns. The width of the output columns is always assumed to be
+  // the maximum width of any school or department element.
+  var numColumns =
+    Math.floor($(document).width() /
+	       (nsYacs.deptColumnWidth + (nsYacs.deptColumnMargin * 2)));
+  var schoolsFinalWidth = numColumns *
+    (nsYacs.deptColumnWidth + (nsYacs.deptColumnMargin * 2));
+  
+  // This application does not know whether there are schools defined in the
+  // database. If there are, the <schools> element will have a nonzero number of
+  // children.
+  var schoolsArray = $('schools').children();
+  var numSchools = schoolsArray.length;
+
+  if(numSchools == 0) {
+    // schools are not defined
+    $('schools').remove();
+    if(numColumns > 1) {
+      // height calculations are much simpler; all we have to do is find a
+      // roughly equal number of departments per column. Still have to do <td>
+      // wrapping though.
+      var columnCtr = 0;
+      var deptsPerColumn =
+	Math.ceil($('departments').children().length / numColumns);
+      var appendLastTD = true;
+      $('department').each(function(i, dept) {
+	if(columnCtr == 0) {
+	  $(dept).before('<td>');
+	}
+	columnCtr++;
+	if(columnCtr == deptsPerColumn) {
+	  $(dept).after('</td>');
+	  columnCtr = 0;
+	}
+      });
+      
+      if(appendLastTD) {
+	$('departments').append('</td>');
+      }
+      $('departments').wrapInner('<table id="homeTable"><tr></tr></table>');
+    }
+  }
+  else {
+    // schools are defined
+    if(numColumns > 1) {
+      // need to calculate the "height" of each school, not in pixels, but in
+      // some arbitrary unit of height independent of the styling.
+      schoolHeights = [];
+      $('school').each(function(i, obj) {
+	// school height = the number of <department> tags within its list plus
+	// 1 for the school heading (assumed to be around the same height as a
+	// department)
+	schoolHeights[i] =
+	  { 'height' : $(obj).find('department').length + 1,
+	    'data' : '<school>'+$(obj).html()+'</school>' };
+      });
+      // Order the schoolHeights array by height
+      schoolHeights.sort(function(x,y) { return x.height < y.height });
+
+      // I'm not implementing the full general solution to this problem (how
+      // best to distribute elements of varying heights evenly in a number of
+      // columns) at this time. The current algorithm is: put them in columns
+      // 1, 2, ..., n; 1, 2, ..., n; and so on until all schools are gone.
+
+      // Iterate over the <school>s and wrap each group of numColumns of them
+      // in a td tag.
+      var columnCtr = 0;
+      var schoolsPerColumn = Math.ceil(numSchools / numColumns);
+      var appendLastTD = true;
+      if(numSchools / numColumns == 0) {
+	appendLastTD = false; // it will be appended by the loop
+      }
+      $('school').each(function(i, school) {
+	if(columnCtr == 0) {
+	  $(school).before('<td>');
+	}
+	columnCtr++;
+	if(columnCtr == schoolsPerColumn) {
+	  $(school).after('</td>');
+	  columnCtr = 0;
+	}
+      });
+      
+      if(appendLastTD) {
+	$('schools').append('</td>');
+      }
+      $('schools').wrapInner('<table id="homeTable"><tr></tr></table>');
+    }
+  }
+  // homeTable is either the child of departments or schools
+  // they are unnecessary containers that get in the way, so remove them from
+  // the DOM
+  $('table#homeTable').unwrap();
+  // page will not center unless homeTable is given a definite width
+  $('table#homeTable').css('width', schoolsFinalWidth);
+  
+  // Add a click event listener to all departments to load that department's
+  // courses from the API
+  $('department').click(function() {
+    var dept = $(this);
+    nsYacs.searchbar.value = dept.children('department-code').html();
+    loadCourses("/api/v5/courses.xml?department_id=" +
+		dept.children('department-id').html());
+    
+  });
+}
+
+// Anything that has to be done when loading up the courses/search results.
+// The string argument is the literal API request that will be made, unchanged.
+// If performing a search, you should call searchToQuery first.
+function loadCourses(apiString) {
+  replaceContent(apiString);
+  formatSearchResults();
 }
 
 /* Given a search string (what the user entered in the search bar), restructure
@@ -67,14 +204,11 @@ function setup() {
   nsYacs.homeButton = document.getElementById("page-title");
   nsYacs.schedButton = document.getElementById("schedule-btn");
   nsYacs.searchbar = document.getElementById("searchbar");
-  
-  // Load the default home page 
-  replaceContent("/api/v5/departments.xml");
 
-  // Add click events to the YACS and schedule buttons
-  nsYacs.homeButton.addEventListener("click", function() {
-    replaceContent("/api/v5/departments.xml");
-  });
+  // Add click event to the YACS button
+  nsYacs.homeButton.addEventListener("click", loadHomePage);
+
+  // Add click event to the schedule button
   nsYacs.schedButton.addEventListener("click", function() {
     // MUST BE REPLACED WITH REAL SCHEDULE API REQUEST
     replaceContent("sampleSchedule.html");
@@ -85,10 +219,14 @@ function setup() {
     if(event.keyCode == 13) {
       var searchURL = "/api/v5/courses.xml?q="+
 	searchToQuery(nsYacs.searchbar.value);
-      replaceContent(searchURL);
-      formatSearchResults();
+      loadCourses(searchURL);
     }
   });
+  
+  // Load the default home page
+  loadHomePage();
+
+  // 
 }
 
 document.addEventListener("DOMContentLoaded", setup, false);
