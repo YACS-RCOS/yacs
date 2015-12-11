e6a357b024338834122b06d6c131c7f4c5b02698
Richie Young <richie@sprocket-208.dynamic2.rpi.edu>
2015-12-11 15:55:24 -0500

    orders courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ActiveRecord::Base
   belongs_to  :department
   has_many    :sections
   validates :name, :number, uniqueness: { scope: :department_id }
+  default_scope { order(number: :asc) }
 end
