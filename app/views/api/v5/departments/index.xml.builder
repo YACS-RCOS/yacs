bd6542e3acdeede1f3b7faf8445eea3991d402e6
Richie <richiejoeyoung@gmail.com>
2016-01-03 12:56:20 -0500

    fixes depts tests

--- app/views/api/v5/departments/index.xml.builder
+++ app/views/api/v5/departments/index.xml.builder
@@ -1,30 +1,32 @@
 xml.instruct!
-xml.tag! 'schools' do
-  if @schools.present?
-    @schools.each do |school|
-      xml.tag! 'school' do
-        # xml.tag! 'school-id', school.id
-        xml.tag! 'school-name', school.name
-        xml.tag! 'departments' do
-          school.departments.each do |department|
-            xml.tag! 'department' do
-              xml.tag! 'department-id', department.id
-              xml.tag! 'department-code', department.code
-              xml.tag! 'department-name', department.name
+xml.tag! 'root' do # FIXME
+  xml.tag! 'schools' do
+    if @schools.present?
+      @schools.each do |school|
+        xml.tag! 'school' do
+          # xml.tag! 'school-id', school.id
+          xml.tag! 'school-name', school.name
+          xml.tag! 'departments' do
+            school.departments.each do |department|
+              xml.tag! 'department' do
+                xml.tag! 'department-id', department.id
+                xml.tag! 'department-code', department.code
+                xml.tag! 'department-name', department.name
+              end
             end
           end
         end
       end
     end
   end
-end
-xml.tag! 'departments' do
-  if @departments.present?
-    @departments.each do |department|
-      xml.tag! 'department' do
-        xml.tag! 'department-id', department.id
-        xml.tag! 'department-code', department.code
-        xml.tag! 'department-name', department.name
+  xml.tag! 'departments' do
+    if @departments.present?
+      @departments.each do |department|
+        xml.tag! 'department' do
+          xml.tag! 'department-id', department.id
+          xml.tag! 'department-code', department.code
+          xml.tag! 'department-name', department.name
+        end
       end
     end
   end
--- spec/requests/api/v5/departments_spec.rb
+++ spec/requests/api/v5/departments_spec.rb
@@ -1,37 +1,39 @@
 describe 'Departments API' do
   it '#index' do
     FactoryGirl.create_list(:department, 10)
-    get '/api/v5/departments.json'
+    get '/api/v5/departments.xml'
     expect(response).to be_success
-    expect(json['departments'].length).to eq 10
+    expect(xml.root.departments.department.length).to eq 10
     Department.all.each_with_index do |dept, n|
-      expect(json['departments'][n]['codename']).to eq dept.code
-      expect(json['departments'][n]['name']).to eq dept.name
+      expect(xml.root.departments.department[n].search('department-code').text).to eq dept.code
+      expect(xml.root.departments.department[n].search('department-name').text).to eq dept.name
     end
   end
 
-  it '#index?use_schools=1' do
-    depts = FactoryGirl.create_list(:department, 2)
-    school = FactoryGirl.create(:school)
-    depts[0].school = school
-    depts[0].save!
-    get '/api/v5/departments.json?use_schools=1'
+  it '#index (with schools)' do
+    depts = FactoryGirl.create_list(:department, 4)
+    schools = FactoryGirl.create_list(:school, 2)
+    depts[0].school = depts[1].school = schools[0]
+    depts[0].save!; depts[1].save!
+    get '/api/v5/departments.xml'
     expect(response).to be_success
-    expect(json['departments']).to be_present
-    expect(json['departments'].length).to eq 1
-    expect(json['departments'][0]['codename']).to eq depts[1].code
-    expect(json['departments'][0]['name']).to eq depts[1].name
-    expect(json['schools']).to be_present
-    expect(json['schools'].length).to eq 1
-    expect(json['schools'][0]['name']).to eq school.name
+    expect(xml.root.departments.try(:department)).to_not be_nil
+    expect(xml.root.departments.department.length).to eq 2
+    expect(xml.root.departments.department[0].search('department-code').text).to eq depts[2].code
+    expect(xml.root.departments.department[0].search('department-name').text).to eq depts[2].name
+    expect(xml.root.schools.try(:school)).to_not be_nil
+    expect(xml.root.schools.school.length).to eq 2
+    expect(xml.root.schools.school[0].search('school-name').text).to eq schools[0].name
+    expect(xml.root.schools.school[0].departments.department[0].search('department-code').text).to eq depts[0].code
+    expect(xml.root.schools.school[0].departments.department[0].search('department-name').text).to eq depts[0].name
   end
 
   it '#show' do
     dept = FactoryGirl.create(:department)
-    get "/api/v5/departments/#{dept.id}.json"
+    get "/api/v5/departments/#{dept.id}.xml"
     expect(response).to be_success
-    expect(json['id']).to eq dept.id
-    expect(json['codename']).to eq dept.code
-    expect(json['name']).to eq dept.name
+    expect(xml.search('department-id').text).to   eq dept.id.to_s
+    expect(xml.search('department-code').text).to eq dept.code
+    expect(xml.search('department-name').text).to eq dept.name
   end
 end
