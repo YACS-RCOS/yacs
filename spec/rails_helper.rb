30b7023fa78d010fdca542442f28faaa94164396
Richie Young <richiejoeyoung@gmail.com>
2016-01-03 12:17:06 -0500

    fixes courses api tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
     end
   end
 
-  config.include Requests::JsonHelpers, type: :request
+  config.include Requests::ParseHelpers, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,7 +1,12 @@
 module Requests
-  module JsonHelpers
+  module ParseHelpers
     def json
       JSON.parse(response.body)
+    end
+    def xml
+      @xml = Nokogiri::Slop(response.body) if response.body != @body
+      @body = response.body
+      @xml
     end
   end
 end
