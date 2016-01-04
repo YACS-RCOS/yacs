391c687e60c0c428a53d1cd708678be0be6ebbdf
Richie <richiejoeyoung@gmail.com>
2016-01-03 22:10:20 -0500

    adds scheduler class and placeholder schedule generator method; adds scheduling controller method

--- app/controllers/api/v5/schedules_controller.rb
+++ app/controllers/api/v5/schedules_controller.rb
@@ -1,5 +1,8 @@
 class Api::V5::SchedulesController < Api::V5::ApiController
   def index
-
+    sections = params[:sections].map do |id|
+      Section.find(id)
+    end
+    @schedules = Schedule::Scheduler.one_schedule(sections)
   end
 end
