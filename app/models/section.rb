a5ca99bed351217277b30f134cf089f10fe9e261
Richie <richiejoeyoung@gmail.com>
2016-01-06 17:24:08 -0500

    adds schedules xml view and conflicts method to sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,18 @@
 class Section < ActiveRecord::Base
   belongs_to  :course
   default_scope { order(name: :asc) }
+
+  def conflicts_with(section)
+    i = 0
+    while i < num_periods
+      j = 0
+      while j < section.num_periods
+        if periods_day[i] == section.periods_day[i] 
+          && ((periods_start[i] <= section.periods_start[j] && periods_end[i] >= section.periods_start[j])
+          || (periods_start[i] >= section.periods_start[j] && periods_start[i] <= section.periods_end[j]))
+          return true
+      end
+    end
+    false
+  end
 end
