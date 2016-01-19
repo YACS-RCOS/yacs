5f33d869eb2dfbc6a942b930e190cddb18bc891e
Richie <richiejoeyoung@gmail.com>
2016-01-18 20:58:37 -0500

    fixes section_ids[] params naming for scheduler api; adds test for when param is not present

--- app/controllers/api/v5/schedules_controller.rb
+++ app/controllers/api/v5/schedules_controller.rb
@@ -1,7 +1,11 @@
 class Api::V5::SchedulesController < Api::V5::ApiController
   def index
-    sections = params[:sections].map do |id|
-      Section.find(id)
+    if params[:section_ids].present?
+      sections = params[:section_ids].map do |id|
+        Section.find(id)
+      end
+    else
+      sections = []
     end
     @schedules = Scheduler.all_schedules(sections)
     respond_to do |format|
