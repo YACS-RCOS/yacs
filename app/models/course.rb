1b8468e17a3e26e51b9dd107fd7cd9323198e29b
Richie <richiejoeyoung@gmail.com>
2016-02-02 02:37:10 -0500

    orders course search result by search rank

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,21 +5,23 @@ class Course < ActiveRecord::Base
   default_scope { order(number: :asc) }
 
   def self.search(params)
+    search_params = params.join(' & ')
     query = <<-SQL
-      SELECT *
-      FROM ( SELECT DISTINCT courses.*,
-        to_tsvector(departments.name) ||
-        to_tsvector(departments.code) ||
-        to_tsvector(to_char(courses.number, '9999')) ||
-        to_tsvector(courses.name) ||
-        to_tsvector(coalesce((string_agg(array_to_string(sections.instructors, ' '), ' ')), ''))
-        AS document
-        FROM courses
+      SELECT * FROM (
+        SELECT DISTINCT
+          courses.*,
+          to_tsvector(departments.name) ||
+          to_tsvector(departments.code) ||
+          to_tsvector(to_char(courses.number, '9999')) ||
+          to_tsvector(courses.name) ||
+          to_tsvector(coalesce((string_agg(array_to_string(sections.instructors, ' '), ' ')), ''))
+        AS document FROM courses
         JOIN sections on sections.course_id = courses.id
         JOIN departments on courses.department_id = departments.id
         GROUP BY courses.id, sections.id, departments.id
       ) c_search
-      WHERE c_search.document @@ to_tsquery('#{params.join(' & ')}');
+      WHERE c_search.document @@ to_tsquery('#{search_params}')
+      ORDER BY ts_rank(c_search.document, to_tsquery('#{search_params}')) DESC;
     SQL
     find_by_sql(query)
   end
