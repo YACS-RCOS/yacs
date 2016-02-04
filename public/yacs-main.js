1eedb71c16fb81d6e53d0460a8826e0d23cde86e
aosdict <aosdict@gmail.com>
2016-02-04 13:16:28 -0500

    Made schedules work with the modified json API

--- public/yacs-main.js
+++ public/yacs-main.js
@@ -1,6 +1,6 @@
 // yacs namespace
 // contains application constants
-var nsYacs = {
+const nsYacs = {
   deptColumnWidth : 600, // should be the same as the width
                          // defined for <school> in yacs-main.css
   deptColumnMargin : 10, // should be the same as side margins defined
@@ -453,22 +453,27 @@ function convertSchedToPeriods(schedData) {
     // assume the length of periods_start is the same as periods_end,
     // periods_type and periods_day. (else it's invalid)
     
-    for (var i=0; i<sect.periods_start.length; ++i) {
-      var period = { // the current period getting added into the structure
-	day       : sect.periods_day[i],
-	start     : roundTo5Min(sect.periods_start[i]),
-	end       : roundTo5Min(sect.periods_end[i]),
-	//prof      : sect.instructors,
-	code      : sect.department_code,
-	courseNum : sect.course_number,
-	sectNum   : sect.name, // Should probably be a better term than "name"
-	                       // but that's a problem with the API
-	type      : sect.periods_type[i],
-	title     : sect.course_name,
-	schedNum  : courseCtr
-      };
+    for (var i=0; i<sect.periods.length; ++i) {
+      // the current period getting added into the structure
+      var period = sect.periods[i]; 
+      //period.prof = sect.instructors,
+      // convert the times involved
+      period.start     = roundTo5Min(period.start);
+      period.end       = roundTo5Min(period.end);
+      period.code      = sect.department_code;
+      period.courseNum = sect.course_number;
+      period.sectNum   = sect.name; // Should be a better term than "name"
+	                          // but that's a problem with the API
+      period.title     = sect.course_name;
+      period.schedNum  = courseCtr;
       
       // use a crude insertion sort based on start time (data set is small)
+      /* If the API promises to sort the periods (by day and then by start
+	 time), we can get rid of this frontend sort and simply append each
+	 period to the end of its day array:
+	 week[period.day].push(period);
+	 13 lines down to 1! :O
+      */
       var inserted = false;
       for(var j=0; j<week[period.day].length; ++j) {
 	if(week[period.day][j].start > period.start) {
