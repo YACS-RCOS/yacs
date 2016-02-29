d73a11ca053557d0e5f41c0290789bfe36ae285f
Richie <richiejoeyoung@gmail.com>
2016-02-28 21:43:00 -0500

    adds pidfile config for production server

--- config/puma.rb
+++ config/puma.rb
@@ -10,6 +10,11 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+if ENV['PIDFILE']
+  pidfile ENV['PIDFILE']
+  daemonize
+end
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
