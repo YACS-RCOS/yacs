8bdbc780e498adf579188ce879f2b28a556f2305
Richie <richiejoeyoung@gmail.com>
2016-03-20 14:28:18 -0400

    switches cache store to dalli/memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.cache_store = :dalli_store
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
