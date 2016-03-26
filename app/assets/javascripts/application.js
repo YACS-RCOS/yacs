541d29c4f9f9d26f672cd6b6c6eba931c74e59c8
Richie <richiejoeyoung@gmail.com>
2016-03-26 02:17:54 -0400

    begins refactoring javascripts and adds handlebars vendored files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require handlebars.runtime
+//= require yacs
 //= require_tree ./templates
 //= require_tree .
 //= require_tree ../../../vendor/assets/javascripts
