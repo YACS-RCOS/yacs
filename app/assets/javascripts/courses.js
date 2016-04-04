22aaced69c2181813e78af9b0b1be45f8d2a0342
aosdict <aosdict@gmail.com>
2016-04-04 19:09:37 -0400

    Fixed seats helper to return the complete string

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,8 +2,7 @@ Handlebars.registerHelper('department_code', function (id) {
   return new Handlebars.SafeString(Yacs.models.departments.store.id[id].code);
 });
 
-Handlebars.registerHelper('range', function (min, max) {
-  var credits = max;
+Handlebars.registerHelper('render_credits', function (min, max) {
   var outString = '';
   // render "credit(s)" properly
   if(min < max) {
@@ -19,8 +18,8 @@ Handlebars.registerHelper('join', function (arr) {
   return new Handlebars.SafeString(arr.join(', '));
 });
 
-Handlebars.registerHelper('subtract', function (a, b) {
-  return new Handlebars.SafeString(a - b);
+Handlebars.registerHelper('render_seats', function (a, b) {
+  return new Handlebars.SafeString((a-b) + ' seat' + ((a-b) === 1 ? '' : 's'));
 });
 
 Yacs.views.courses = function (data) {
