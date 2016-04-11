f4ba7d77a8d1239ad3fc5c33276be9de3638c796
Aesa Kamar <Aesa.Kamar@Gmail.com>
2016-04-11 13:10:24 -0400

    Finally readable console logging with awesome_print

--- config/initializers/catalog.rb
+++ config/initializers/catalog.rb
@@ -3,3 +3,4 @@ Rails.application.config.catalog_adapter = Catalog::RpiAdapter.new
 # This is used to diff models when updating from the catalog
 require 'active_record/diff'
 require 'awesome_print'
+AwesomePrint.irb!
