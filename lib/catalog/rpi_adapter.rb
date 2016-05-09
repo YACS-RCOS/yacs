e873abb281148fb6ce4a2d72fd1822f825dc7ac5
Aesa Kamar <Aesa.Kamar@Gmail.com>
2016-05-09 13:41:33 -0400

    Cleaned up code for readibility and organization. Modified rake task for catalog actions

--- lib/catalog/rpi_adapter.rb
+++ lib/catalog/rpi_adapter.rb
@@ -34,33 +34,51 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
   # LOAD ACTIONS
   # ===============================================
   def load_departments
-    # Gets a list of db departments,
-    # Gets a list of xml departments,
-    dbDepartments = Set.new Department.all.map { |x| { code: x.code, name: x.name } }
-    xmlDepartments = Set.new
+    # Initialize environment
     file = 'departments.txt'
+
+    # Display formatting
     puts "\e[95m" << 'Departments' << "\e[0m"
     progressbar = ProgressBar.create(total: File.readlines(file).length)
+
+    # Create sets to hold entities for logging and debug
+    dbDepartments   = Set.new Department.all.map { |x| { code: x.code, name: x.name } }
+    xmlDepartments  = Set.new
+    modDepartments  = Set.new
+    errDepartments  = Set.new
+
     File.readlines(file).drop(1).each do |line|
       progressbar.increment
+
+      # Read the data from the XML and track it
       data = line.strip.split(/\t/)
-      # build the list of XML departments
       tmpDepartment = Department.new(code: data[0], name: data[1])
       xmlDepartments << { code: tmpDepartment.code, name: tmpDepartment.name }
 
-      # Save unless found, where different
+      # Update or Create
       fndDepartment = Department.where(name: tmpDepartment.name, code: tmpDepartment.code).first
-      if fndDepartment
-        fndDepartment.update!(tmpDepartment.attributes.except('id', 'created_at', 'updated_at')) if fndDepartment.diff?(tmpDepartment)
-      else
-        tmpDepartment.save!
+      begin
+        if fndDepartment
+          if fndDepartment.diff?(tmpDepartment)
+            if fndDepartment.update!(tmpDepartment.attributes.except('id', 'created_at', 'updated_at'))
+              modDepartments << { code: tmpDepartment.code, name: tmpDepartment.name }
+            end
+          end
+        else
+          tmpDepartment.save!
+        end
+      rescue
+        errDepartments << { code: tmpDepartment.code, name: tmpDepartment.name }
       end
     end
+
     # Compare lists, destroy old records if found
     progressbar.finish
     toReject = dbDepartments - xmlDepartments
-    puts "\tCreated+ " << (xmlDepartments - dbDepartments).length.to_s
-    puts "\tDeleted- " << toReject.length.to_s
+    puts "\tCreated + " << (xmlDepartments - dbDepartments).length.to_s
+    puts "\tDeleted - " << toReject.length.to_s
+    puts "\tModified~ " << modDepartments.length.to_s
+    puts "\tErrored ! " << errDepartments.length.to_s
     puts "\t\e[36m" << 'Total= ' << "\e[0m" << Department.count.to_s
     if toReject
       toReject.each do |dpt|
@@ -70,25 +88,41 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
   end
 
   def load_schools
-    # Gets a list of db schools,
-    # Gets a list of xml schools,
-    dbSchools = Set.new School.all.map { |x| { name: x.name } }
-    xmlSchools = Set.new
+    # Initialize environment
     file = 'schools.txt'
+
+    # Display formatting
     puts "\e[95m" << 'Schools' << "\e[0m"
     progressbar = ProgressBar.create(total: File.readlines(file).length)
+
+    # Create sets to hold entities for logging and debug
+    dbSchools   = Set.new School.all.map { |x| { name: x.name } }
+    xmlSchools  = Set.new
+    modSchools  = Set.new
+    errSchools  = Set.new
+
     File.readlines(file).each do |line|
       progressbar.increment
+
+      # Read the data from the XML and track it
       data = line.strip.split(/\t/)
       tmpSchool = School.new(name: data[0])
       xmlSchools << { name: tmpSchool.name }
 
-      # Save unless found, where different
+      # Update or Create
       fndSchool = School.where(name: tmpSchool.name).first
-      if fndSchool
-        fndSchool.update!(tmpSchool.attributes.except('id', 'created_at', 'updated_at')) if fndSchool.diff?(tmpSchool)
-      else
-        tmpSchool.save!
+      begin
+        if fndSchool
+          if fndSchool.diff?(tmpSchool)
+            if fndSchool.update!(tmpSchool.attributes.except('id', 'created_at', 'updated_at'))
+              modSchools << { name: tmpSchool.name }
+            end
+          end
+        else
+          tmpSchool.save!
+        end
+      rescue
+        errSchools << { name: tmpSchool.name }
       end
 
       data.drop(1).each do |code|
@@ -96,11 +130,14 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
         tmpDepartment.update!(school_id: tmpSchool.id) if tmpDepartment.school_id != tmpSchool.id
       end
     end
-    progressbar.finish
+
     # Compare lists, destroy old records if found
+    progressbar.finish
     toReject = dbSchools - xmlSchools
-    puts "\tCreated+ " << (xmlSchools - dbSchools).length.to_s
-    puts "\tDeleted- " << toReject.length.to_s
+    puts "\tCreated + " << (xmlSchools - dbSchools).length.to_s
+    puts "\tDeleted - " << toReject.length.to_s
+    puts "\tModified~ " << modSchools.length.to_s
+    puts "\tErrored ! " << errSchools.length.to_s
     puts "\t\e[36m" << 'Total= ' << "\e[0m" << School.count.to_s
     if toReject
       toReject.each do |sch|
@@ -110,36 +147,29 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
   end
 
   def load_courses_and_sections
+    # Initialize environment
     uri = "https://sis.rpi.edu/reg/rocs/#{sem_string}.xml"
     @courses_xml = Nokogiri::XML(open(uri)).xpath('//COURSE')
-
-    # Compose lists of courses and sections for comparison later
-    errCourses = Set.new
-    dbCourses = Set.new Course.all.includes(:sections).map do |crs|
-      {
-        number: crs.number,
-        department_id: crs.department_id
-      }
-    end
-    xmlCourses = Set.new
-
-    errSections= Set.new
-    dbSections = Set.new Section.all.map do |sec|
-      {
-        name: sec.name,
-        crn: sec.crn,
-        course_id: sec.course_id
-      }
-    end
-    xmlSections = Set.new
 
     # Display formatting about what we are doing
     puts "\e[95m" << 'Courses and Sections' << "\e[0m"
     progressbar = ProgressBar.create(total: @courses_xml.length)
 
+    # Create sets to hold entities for logging and debug
+    dbCourses   = Set.new Course.all.includes(:sections).map { |crs| { number: crs.number, department_id: crs.department_id } }
+    xmlCourses  = Set.new
+    modCourses  = Set.new
+    errCourses  = Set.new
+
+    dbSections  = Set.new Section.all.map { |sec| { name: sec.name, crn: sec.crn, course_id: sec.course_id } }
+    xmlSections = Set.new
+    modSections = Set.new
+    errSections = Set.new
+
     @courses_xml.each do |course_xml|
       progressbar.increment
 
+      # Read the data from the XML and track it
       dept = Department.where(code: course_xml[:dept])[0]
       tmpCourse              = dept.courses.build
       tmpCourse.name         = course_xml[:name].titleize
@@ -148,38 +178,43 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
       tmpCourse.max_credits  = course_xml[:credmax]
       xmlCourses << { number: tmpCourse.number, department_id: dept.id }
 
+      # Update or Create
       fndCourse = Course.includes(:sections).where(number: tmpCourse.number, department_id: tmpCourse.department_id).first
       sections_xml = course_xml.xpath('SECTION')
       if !fndCourse.nil?
         if fndCourse.diff?(tmpCourse)
-          unless fndCourse.update!(tmpCourse.attributes.except('id', 'name', 'created_at', 'updated_at'))
+          if fndCourse.update!(tmpCourse.attributes.except('id', 'name', 'created_at', 'updated_at'))
+            modCourses << { number: tmpCourse.number, department_id: dept.id }
+            # Load sections from fndCourse
+            sections_xml.each do |section_xml|
+              load_section(fndCourse, section_xml, xmlSections, modSections, errSections)
+            end
+          else
             errCourses << { number: tmpCourse.number, department_id: dept.id }
           end
         end
-        sections_xml.each do |section_xml|
-          load_section(fndCourse, section_xml, xmlSections, errSections)
-        end
       else
-        if !tmpCourse.save
-          errCourses << { number: tmpCourse.number, department_id: dept.id }
-        else
+        if tmpCourse.save
+          # Load sections from tmpCourse
           sections_xml.each do |section_xml|
-            load_section(tmpCourse, section_xml, xmlSections, errSections)
+            load_section(tmpCourse, section_xml, xmlSections, modSections, errSections)
           end
+        else
+          errCourses << { number: tmpCourse.number, department_id: dept.id }
         end
       end
     end
+
     # PRINT REJECTED COURSES
-    if errCourses
-      # puts "There were #{errCourses.length} errors"
-      # ap errCourses
-    end
     progressbar.finish
+    puts '==============='
+    puts 'Courses'
     # Compare lists, destroy old records if found
     toRejectCourses = (dbCourses - xmlCourses)- errCourses
-    puts "\tCreated+ " << ((xmlCourses - dbCourses) - errCourses).length.to_s
-    puts "\tDeleted- " << toRejectCourses.length.to_s
-    puts "\tErrors!  " << errCourses.length.to_s
+    puts "\tCreated + " << ((xmlCourses - dbCourses) - errCourses).length.to_s
+    puts "\tDeleted - " << toRejectCourses.length.to_s
+    puts "\tModified~ " << modCourses.length.to_s
+    puts "\tErrored ! " << errCourses.length.to_s
     puts "\t\e[36m" << 'Total= ' << "\e[0m" << Course.count.to_s
     if toRejectCourses
       toRejectCourses.each do |crs|
@@ -193,9 +228,10 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
     puts 'Sections'
     # ap dbSections
     toRejectSections = (dbSections - xmlSections) - errSections
-    puts "\tCreated+ " << ((xmlSections - dbSections)- errSections).length.to_s
-    puts "\tDeleted- " << toRejectSections.length.to_s
-    puts "\tErrors!  " << errSections.length.to_s
+    puts "\tCreated + " << ((xmlSections - dbSections)- errSections).length.to_s
+    puts "\tDeleted - " << toRejectSections.length.to_s
+    puts "\tModified~ " << modSections.length.to_s
+    puts "\tErrored ! " << errSections.length.to_s
     puts "\t\e[36m" << 'Total= ' << "\e[0m" << Section.count.to_s
     # ap toRejectSections
     if toRejectSections
@@ -205,15 +241,18 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
     end
   end
 
-  def load_section(course, section_xml, xmlSections, errSections)
+  def load_section(course, section_xml, xmlSections, modSections, errSections)
+    # Read the data from the XML and track it
     tmpSection             = Section.new({course_id: course.id})
     tmpSection.name        = section_xml[:num]
     tmpSection.crn         = section_xml[:crn]
     tmpSection.seats       = section_xml[:seats]
     tmpSection.seats_taken = section_xml[:students]
     tmpSection.num_periods = 0
-    periods_xml = section_xml.xpath('PERIOD')
+    periods_xml            = section_xml.xpath('PERIOD')
     tmpSection.instructors = instructors = []
+
+    # Build a list of periods from xmlSections
     periods_xml.each do |period_xml|
       tmpSection.instructors.concat(period_xml[:instructor].strip.split(/\//))
       days_xml = period_xml.xpath('DAY')
@@ -225,9 +264,11 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
         tmpSection.periods_type  .push(period_xml[:type])
       end
     end
+    # Validate the section
     tmpSection.instructors.delete('Staff')
     tmpSection.instructors.uniq!
     xmlSections << { name: tmpSection.name, crn: tmpSection.crn, course_id: course.id }
+
     # Update or create
     fndSection = Section.where(name: tmpSection.name, crn: tmpSection.crn, course_id: tmpSection.course_id).first
     # We want to skip this if we don't have a full match, it'll be created later
@@ -237,7 +278,9 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
     end
     if fndSection
       if fndSection.diff?(tmpSection)
-        unless fndSection.update(tmpSection.attributes.except('id', 'created_at', 'updated_at'))
+        if fndSection.update(tmpSection.attributes.except('id', 'created_at', 'updated_at'))
+          modSections << { name: tmpSection.name, crn: tmpSection.crn, course_id: course.id }
+        else
           errSections << { name: tmpSection.name, crn: tmpSection.crn, course_id: course.id }
         end
       end
@@ -249,52 +292,72 @@ class Catalog::RpiAdapter < Catalog::AbstractAdapter
   end
 
   def load_descriptions
-  base = 'http://catalog.rpi.edu/'
-  page_no = 1
-  xmlDescriptions=[]
+    # Initialize environment
+    base = 'http://catalog.rpi.edu/'
+    page_no = 1
 
-  puts "\e[95m" <<"Descriptions" << "\e[0m"
-  progressbar = ProgressBar.create(total: Course.count)
-  while page_no <= 20
-    path = 'content.php?catoid=14&catoid=14&navoid=336&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=' + page_no.to_s + '#acalog_template_course_filter'
-    page = base + path
-    page = Nokogiri::HTML(open(page))
-    page_no += 1
-    rows = page.css('td.block_content table tr')
-    rows[1..-2].each do |row|
-      courses = row.css('td a').to_a.compact
-      courses.each do |course|
-        href = course['href']
-        course_title = course.text.sub(/-.*/, '').strip.split
-        course_name = course.text.split('-')[1..-1].join('-').gsub(/[^ -\~]/, '').strip
-        course_model = Course.where(number: course_title[1]).includes(:department).where(departments: { code: course_title[0] }).first
-        next unless course_model
-        progressbar.increment
-        desc_path = base + href# + '&print'
-        # FIXME THIS TAKES A LONG TIME vvvvvvvv
-        desc_page = Nokogiri::HTML(open(desc_path))
-        # FIXME THIS TAKES A LONG TIME ^^^^^^^^
-        desc_page.search('h1').remove
-        desc = desc_page.css('td.block_content')
-        course_description = desc.text
-        course_description.slice! 'HELP'
-        course_description.slice! 'Rensselaer Catalog 2015-2016'
-        course_description.slice! 'Print-Friendly Page [Add to Portfolio]'
-        course_description.slice! ' Back to Top | Print-Friendly Page [Add to Portfolio]'
-        course_description = course_description.strip
-        if course_description.present? && (course_model.description != course_description)
-          course_model.update!(description: course_description)
-          xmlDescriptions <<  "#{course_title} - #{course_description}"
-        end
-        if (course_name.present? && course_model.name != course_name)
-          course_model.update!(name: course_name)
+    # Display formatting
+    puts "\e[95m" <<"Descriptions" << "\e[0m"
+    progressbar = ProgressBar.create(total: Course.count)
+
+    # Sets for debug and logging
+    xmlDescriptions = []
+    modCourses      = Set.new
+
+    while page_no <= 20
+      # Initialize HTML scraper
+      path = 'content.php?catoid=14&catoid=14&navoid=336&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=' + page_no.to_s + '#acalog_template_course_filter'
+      page = base + path
+      page = Nokogiri::HTML(open(page))
+      page_no += 1
+      rows = page.css('td.block_content table tr')
+
+      # For each matched element
+      rows[1..-2].each do |row|
+        courses = row.css('td a').to_a.compact
+        # For each course, modify the descriptions
+        courses.each do |course|
+          href = course['href']
+          course_title = course.text.sub(/-.*/, '').strip.split
+          course_name = course.text.split('-')[1..-1].join('-').gsub(/[^ -\~]/, '').strip
+          course_model = Course.where(number: course_title[1]).includes(:department).where(departments: { code: course_title[0] }).first
+          next unless course_model
+          progressbar.increment
+
+          # Once course is found to exist in DB, attempt to modify from web data
+          desc_path = base + href# + '&print'
+          # FIXME THIS TAKES A LONG TIME vvvvvvvv
+          desc_page = Nokogiri::HTML(open(desc_path))
+          # FIXME THIS TAKES A LONG TIME ^^^^^^^^
+          desc_page.search('h1').remove
+          desc = desc_page.css('td.block_content')
+          course_description = desc.text
+          course_description.slice! 'HELP'
+          course_description.slice! 'Rensselaer Catalog 2015-2016'
+          course_description.slice! 'Print-Friendly Page [Add to Portfolio]'
+          course_description.slice! ' Back to Top | Print-Friendly Page [Add to Portfolio]'
+          course_description = course_description.strip
+
+          # Update descriptions
+          if course_description.present? && (course_model.description != course_description)
+            course_model.update!(description: course_description)
+            xmlDescriptions << { course_number: course_model.number, course_description: course_description }
+          end
+
+          # Update course names if needed
+          if course_name.present? && course_model.name != course_name
+            modCourses << { course_number: course_model.number, course_name: course_name }
+            course_model.update!(name: course_name)
+          end
+
         end
       end
     end
+
+    progressbar.finish
+    puts "\tDescribed ~ " << xmlDescriptions.length.to_s
+    puts "\tRenamed   ~ " << modCourses.length.to_s
   end
-  progressbar.finish
-  puts "\tCreated+ " << xmlDescriptions.length.to_s
-end
 
 
   # CLEANUP ACTIONS
--- lib/tasks/catalog.rake
+++ lib/tasks/catalog.rake
@@ -1,14 +1,11 @@
 namespace :catalog do
   task load: :environment do
     Rails.application.config.catalog_adapter.load_catalog
+    ap 'Successfully loaded!'
   end
 
   task destroy: :environment do
     Rails.application.config.catalog_adapter.destroy_catalog
     ap 'Successfully destroyed!'
-  end
-
-  task update_seats: :environment do
-    Rails.application.config.catalog_adapter.update_section_seats
   end
 end
