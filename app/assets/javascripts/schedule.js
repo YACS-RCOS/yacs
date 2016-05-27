b165808f5473fbdbbf74dfc546efdf6654ebef12
Richie <richiejoeyoung@gmail.com>
2016-05-27 00:37:40 -0400

    adds new schedule javascript and css and updates handlebars template

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,107 +1,91 @@
-/* schedules.js - file for the code to handle everything with the schedule
-   page */
+var Schedule = function (scheduleContainer, options) {
+  var self = this;
+  
+  var NUM_COLORS        = 7;
+  var TEXT_COLORS       = ['#d1265d', '#d9652b', '#bf8a2e', '#008a2e', '#1577aa', '#853d80', '#9d5733'];
+  var BACKGROUND_COLORS = ['#ffd4df', '#ffe9cf', '#fff4d0', '#dcf7da', '#ceeffc', '#f7e2f7', '#ede6df'];
+  var BORDER_COLORS     = ['#ff2066', '#ff9332', '#ffcb45', '#48da58', '#00aff2', '#d373da', '#a48363'];
+  var SELECTED_COLORS   = ['#ff3575', '#ff9c46', '#ffcf56', '#59dc68', '#19b5f2', '#d57fdd', '#ac8f71'];
+  /*                         PINK       ORANGE     YELLOW     GREEN      BLUE       PURPLE     BROWN  */
+  
+  options = options || {};
+  options.timeBegin = options.timeBegin || 8 * 60;
+  options.timeSpan  = options.timeSpan  || 12 * 60;
+  options.dayBegin  = options.dayBegin  || 1;
+  options.daySpan   = options.daySpan   || 5;
+  options.gridSize  = options.gridSize  || 60;
+  
+  var scheduleElement = document.createElement('schedule-view');
+  var legendElement   = document.createElement('schedule-legend');
+  var gridElement     = document.createElement('schedule-grid');
 
-/* Conversion function of military times to "minutes since midnight" format.
-   THIS IS A TEMPORARY FUNCTION. When the backend part of Issue #102 is done,
-   the times passed in the API will already be in this format. This function and
-   any call to it should be deleted. */
-function convertMilTime(milTime) {
-  return Math.floor(milTime/100)*60 + (milTime % 100);
-}
+  scheduleElement.appendChild(legendElement);
+  scheduleElement.appendChild(gridElement);
+  scheduleContainer.appendChild(scheduleElement);
+  
+  var daySize = function (n) {
+    return n * (100 / options.daySpan) + '%';
+  };
 
-/* Given a single schedule JSON object as it is retrieved from the schedule API,
-   return a new object that follows this structure:
-   {
-      startHour: int, // these are 24-hour hours so e.g. 15 is possible
-      endHour: int,
-      week: {
-        Sunday: [
-          // List of periods that will be sorted by their start field
-          {
-            start: int,
-            end: int,
-            deptCode: string,
-            courseNum: string,
-            sectNum: string,
-            title: string,
-            remaining: string,
-            schedNum: int       // used to identify all sections of one course
-          },
-          {...},
-          ...
-        ],
-        Monday: [
-          {...}, ...
-        ],
-        ...
-      }
-   }
-*/
-function convertSchedToPeriods(data) {
-  // Temporary variable that will hold the weekday lists.
-  // This is so they can be referred to by index.
-  var tmpweek = [[], [], [], [], [], [], []];
+  var timeSize = function (n) {
+    return n * (100 / options.timeSpan) + '%';
+  };
+  
+  self.addEvent = function (event) {
+    var eventText       = document.createElement('event-text');
+    var eventElement    = document.createElement('schedule-event');
+    var eventBackground = document.createElement('event-background');
+    var colorIndex      = Math.floor(event.type % NUM_COLORS);
+    
+    eventText.textContent                 = event.name;
+    eventText.style.color                 = TEXT_COLORS[colorIndex];
+    eventElement.style.top                = timeSize(event.start - options.timeBegin);
+    eventElement.style.left               = daySize(event.day - options.dayBegin);
+    eventElement.style.width              = 'calc(' + daySize(1) + ' - 4px)';
+    eventElement.style.height             = 'calc(' + timeSize(event.end - event.start) + ' - 2px)';
+    eventElement.style.borderColor        = BORDER_COLORS[colorIndex];
+    eventBackground.style.backgroundColor = BACKGROUND_COLORS[colorIndex];
 
-  var eStart = 23;
-  var lEnd = 0;
+    eventElement.appendChild(eventBackground);
+    eventElement.appendChild(eventText);
+    scheduleElement.appendChild(eventElement);
+  };
+  
+  var drawLegend = function () {
+    for (var r = 1; r < options.timeSpan / options.gridSize; ++r) {
+      var hourElement = document.createElement('legend-hour');
+      var hour = options.timeBegin / options.gridSize + r;
 
-  // identifies which section a period belongs to
-  // (used to color all periods of a course the same color)
-  var sectionCtr = 1;
+      if      (hour == 0)  hour = '12 AM';
+      else if (hour == 12) hour = 'Noon';
+      else if (hour < 12)  hour = hour + ' AM';
+      else                 hour = hour - 12 + ' PM';
 
-  for (var s=0; s<data.sections.length; ++s) {
-    var sect = data.sections[s];
-    // assume the length of periods_start is the same as periods_end,
-    // periods_type and periods_day. (else it's invalid)
-
-    for (var i=0; i<sect.periods.length; ++i) {
-      // the current period getting added into the structure
-      var period = sect.periods[i];
-      //period.prof = sect.instructors,
-      // convert the times involved
-      period.start = convertMilTime(period.start);
-      period.end = convertMilTime(period.end);
-      period.code      = sect.department_code;
-      period.courseNum = sect.course_number;
-      period.sectNum   = sect.name; // Should be a better term than "name"
-	                            // but that's a problem with the API
-      period.title     = sect.course_name;
-      period.remaining = sect.seats - sect.seats_taken;
-      period.schedNum  = sectionCtr;
-
-      // add to the correct list
-      tmpweek[period.day].push(period);
-
-      // set eStart and lEnd properly
-      var startHr = Math.floor(period.start/60);
-      var endHr = Math.ceil(period.end/60);
-      if(startHr < eStart) eStart = startHr;
-      if(endHr > lEnd) lEnd = endHr;
+      hourElement.textContent = hour;
+      hourElement.style.top = 'calc(' + timeSize(options.gridSize * r) + ' - 0.75em)';
+      legendElement.appendChild(hourElement);
     }
+  };
 
-    // finished with section so increment the counter
-    sectionCtr++;
-  }
+  var drawGrid = function () {
+    for (var d = 0; d < options.daySpan; ++d) {
+      var dayElement = document.createElement('grid-day');
+      dayElement.style.width = daySize(1);
 
-  // sort the periods in each day by start time
-  for (var i = 0; i < tmpweek.length; i++) {
-    tmpweek[i].sort(function(a,b) { return a.start < b.start; });
-  }
-  console.log(tmpweek);
-}
+      for (var r = 0; r < options.timeSpan / options.gridSize; ++r) {
+        var hourElement = document.createElement('grid-hour');
+        hourElement.style.height = timeSize(options.gridSize);
+        dayElement.appendChild(hourElement);
+      }
 
-Yacs.views.schedule = function(data) {
-  /* data is directly from the API and is of the format
-     { schedules: []}
+      gridElement.appendChild(dayElement);
+    }
+  };
+  
+  drawLegend();
+  drawGrid();
+};
 
-  */
-  // currently there are no other fields besides schedules so convert
-  // directly down to the array
-  var allSchedulesArray = data.schedules;
-  if(allSchedulesArray.length < 1) {
-    console.log('No schedules could be found. Perhaps there is a conflict?');
-    return;
-  }
-
-  convertSchedToPeriods(allSchedulesArray[0]);
-}
+var sched = new Schedule(document.querySelector('#schedule-container'));
+for (var p in periods)
+  sched.addEvent(periods[p]);
