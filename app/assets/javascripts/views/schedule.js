9b76b4bd266826f143660ddf3c4fef5364cf693e
Richie <richiejoeyoung@gmail.com>
2016-05-28 15:45:24 -0400

    adds prelim code to display 1 schedule

--- app/assets/javascripts/views/schedule.js
+++ app/assets/javascripts/views/schedule.js
@@ -5,9 +5,32 @@
  * @memberOf Yacs.views
  */
 Yacs.views.schedule = function (data) {
+  // this function will be deprecated when backend is updated to use minutes-to-midnight format
+  var toMinutes = function (timeString) {
+    var int = parseInt(timeString);
+    return Math.floor(int / 100) * 60 + int % 100;
+  }
+
+// NONE OF THIS IS DONE!
+  var prepareData = function (schedule) {
+    var events = [];
+    schedule.sections.forEach(function (s) {
+      s.periods.forEach(function (p) {
+        events.push({
+          start: toMinutes(p.start),
+          end: toMinutes(p.end),
+          day: p.day,
+          type: s.course_number % 7, // TODO: do this correctly
+          name: s.department_code + ' ' + s.course_number + ' - ' + s.name
+        });
+      });
+    });
+    return events;
+  };
+
   Yacs.setContents(HandlebarsTemplates.schedule());
   var scheduleContainer = document.querySelector('#scheduleContainer');
   var schedule = new Schedule(scheduleContainer);
-  // if (data.schedules.length)
-  //   schedule.addEvents(data.schedules[0]);
+  var events = prepareData(data.schedules[0]);
+  events.forEach (function (e) { schedule.addEvent(e); });
 }
