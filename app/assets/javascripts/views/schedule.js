f64ad6fa9b12b61a97751a8d3ab6ff65840df39f
ren <luoh2@rpi.edu>
2016-06-23 16:24:37 -0400

    changed the indentation

--- app/assets/javascripts/views/schedule.js
+++ app/assets/javascripts/views/schedule.js
@@ -68,9 +68,9 @@ Yacs.views.schedule = function (data) {
     showSchedule(scheduleIndex);
   });
   Yacs.on('click', clearSwitchElement, function () {
-      schedule.clearEvents();
-      scheduleNumElement.textContent = 0;
-      crnListElement.textContent = "";
+	  schedule.clearEvents();
+	  scheduleNumElement.textContent = 0;
+	  crnListElement.textContent = "";
   });
 
   showSchedule(scheduleIndex);
