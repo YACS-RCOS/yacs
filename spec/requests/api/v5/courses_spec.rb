d53e0fc71a522c523ebe110d53f54817a9ae9104
Richard Young <richiejoeyoung@gmail.com>
2016-07-08 22:56:15 -0400

    remove xml tests

--- spec/requests/api/v5/courses_spec.rb
+++ spec/requests/api/v5/courses_spec.rb
@@ -1,12 +1,3 @@
-def match_courses(courses, dept)
-  courses.each_with_index do |course, n|
-    expect(xml.courses.course[n].search('course-id').text) .to eq course.id.to_s
-    expect(xml.courses.course[n].search('course-name').text) .to eq course.name.to_s
-    expect(xml.courses.course[n].search('course-number').text) .to eq course.number.to_s
-    expect(xml.courses.course[n].search('department-code').text) .to eq dept.code.to_s
-  end
-end
-
 def json_validate_courses(courses=@courses, sections=false, periods=false)
   courses.each_with_index do |course, n|
     ['id', 'name', 'number', 'min_credits', 'max_credits', 'description', 'department_id'].each do |field|
@@ -32,68 +23,6 @@ describe 'Courses API' do
     before do
       @depts = FactoryGirl.create_list(:department, 3)
     end
-
-    it '#index' do
-      courses = FactoryGirl.create_list(:course, 10, department: @depts[0])
-      get '/api/v5/courses.xml'
-      expect(response)    .to be_success
-      expect(xml.courses.course.length) .to eq courses.length
-      match_courses(courses, @depts[0])
-
-      # get '/api/v5/courses.json'
-      # expect(response) .to be_success
-      # expect(json['courses'].length) .to eq courses.length
-      # json_match_courses(courses)
-    end
-
-    context '#index?department_id=' do
-      before do
-        @courses1 = FactoryGirl.create_list(:course, 5, department: @depts[0])
-        @courses2 = FactoryGirl.create_list(:course, 5, department: @depts[1])
-      end
-
-      it 'returns no courses' do
-        get "/api/v5/courses.xml?department_id=#{@depts[2].id}" #@depts[2] should have no courses
-        expect(response).to be_success
-        expect(xml.courses.try(:course)) .to be_nil
-
-        # get "/api/v5/courses.json?department_id=#{@depts[2].id}"
-        # expect(response).to be_success
-        # expect(json['courses']) .to be_empty
-      end
-
-      it "returns the correct courses" do
-        get "/api/v5/courses.xml?department_id=#{@depts[0].id}" #@depts[0,1] should have 5 courses each
-        expect(response)    .to be_success
-        expect(xml.courses.course.length) .to eq @depts[0].courses.length
-        match_courses(@courses1, @depts[0])
-
-        get "/api/v5/courses.xml?department_id=#{@depts[1].id}" #@depts[0,1] should have 5 courses each
-        expect(response)    .to be_success
-        expect(xml.courses.course.length) .to eq @depts[1].courses.length
-        match_courses(@courses2, @depts[1])
-
-        # get "/api/v5/courses.json?department_id=#{@depts[0].id}" #@depts[0,1] should have 5 courses each
-        # expect(response)    .to be_success
-        # expect(json['courses'].length) .to eq @depts[0].courses.length
-        # json_match_courses(@courses1)
-
-        # get "/api/v5/courses.json?department_id=#{@depts[1].id}" #@depts[0,1] should have 5 courses each
-        # expect(response)    .to be_success
-        # expect(json['courses'].length) .to eq @depts[1].courses.length
-        # json_match_courses(@courses2)
-      end
-    end
-
-    # it '#show' do
-    #   course = FactoryGirl.create(:course)
-    #   get "/api/v5/courses/#{course.id}.xml"
-    #   expect(response).to be_success
-    #   expect(xml.search('course-id').text)     .to eq course.id.to_s
-    #   expect(xml.search('course-number').text) .to eq course.number.to_s
-    #   expect(xml.search('course-name').text)   .to eq course.name.to_s
-    # end
-  end
 
   context "there are departments with courses with sections" do
     before do
--- spec/requests/api/v5/sections_spec.rb
+++ spec/requests/api/v5/sections_spec.rb
@@ -1,17 +1,3 @@
-def match_sections(sections, course)
-  sections.each_with_index do |section, n|
-    expect(xml.sections.section[n].search('section-id').text)          .to eq section.id.to_s
-    expect(xml.sections.section[n].search('section-crn').text)         .to eq section.crn.to_s
-    expect(xml.sections.section[n].search('section-name').text)        .to eq section.name.to_s
-    expect(xml.sections.section[n].search('section-seats').text)       .to eq section.seats.to_s
-    expect(xml.sections.section[n].search('section-seats-taken').text) .to eq section.seats_taken.to_s
-    expect(xml.sections.section[n].search('course-id').text)           .to eq course.id.to_s
-    # section.periods.each_with_index do |period, nn|
-    #   expect(xml.sections.section[n].periods.period[nn].search('period-day').text) .to eq period.day
-    # end
-  end
-end
-
 def json_validate_sections(sections=@sections, periods=false)
   sections.each_with_index do |section, n|
     ['id', 'name', 'instructors', 'crn', 'seats', 'seats_taken', 'num_periods'].each do |field|
@@ -31,70 +17,6 @@ def json_validate_sections(sections=@sections, periods=false)
 end
 
 describe 'Sections API' do
-  context 'when there is a course' do
-    before do
-      @courses = FactoryGirl.create_list(:course, 3)
-    end
-
-    it '#index' do
-      sections = FactoryGirl.create_list(:section, 10, course: @courses[0])
-      get '/api/v5/sections.xml'
-      expect(response)    .to be_success
-      expect(xml.sections.section.length) .to eq sections.length
-      match_sections(sections, @courses[0])
-
-      # get '/api/v5/sections.json'
-      # expect(response) .to be_success
-      # expect(json['sections'].length) .to eq sections.length
-      # json_match_sections(sections)
-    end
-
-    context '#index?course_id=' do
-      before do
-        @sections1 = FactoryGirl.create_list(:section, 5, course: @courses[0])
-        @sections2 = FactoryGirl.create_list(:section, 5, course: @courses[1])
-      end
-
-      it 'returns no sections' do
-        get "/api/v5/sections.xml?course_id=#{@courses[2].id}" #@courses[2] should have no sections
-        expect(response).to be_success
-        expect(xml.sections.try(:section))    .to be_nil
-
-        # get "/api/v5/sections.json?course_id=#{@courses[2].id}" #@courses[2] should have no sections
-        # expect(response).to be_success
-        # expect(json['sections'].length) .to eq 0
-      end
-
-      it "returns the correct sections" do
-        get "/api/v5/sections.xml?course_id=#{@courses[0].id}" #@courses[0,1] should have 5 sections each
-        expect(response)    .to be_success
-        expect(xml.sections.section.length) .to eq 5
-        match_sections(@sections1, @courses[0])
-        get "/api/v5/sections.xml?course_id=#{@courses[1].id}" #@courses[0,1] should have 5 sections each
-        expect(response)    .to be_success
-        expect(xml.sections.section.length) .to eq 5
-        match_sections(@sections2, @courses[1])
-
-        # get "/api/v5/sections.json?course_id=#{@courses[0].id}" #@courses[0,1] should have 5 sections each
-        # expect(response)    .to be_success
-        # expect(json['sections'].length) .to eq 5
-        # json_match_sections(@sections1)
-        # get "/api/v5/sections.json?course_id=#{@courses[1].id}" #@courses[0,1] should have 5 sections each
-        # expect(response)    .to be_success
-        # expect(json['sections'].length) .to eq 5
-        # json_match_sections(@sections2)
-      end
-    end
-
-    # it '#show' do
-    #   section = FactoryGirl.create(:section)
-    #   get "/api/v5/sections/#{section.id}.xml"
-    #   expect(response).to be_success
-    #   expect(xml.search('section-id').text) .to eq section.id.to_s
-    #   expect(xml.search('section-crn').text) .to eq section.crn.to_s
-    # end
-  end
-
   context "there are courses with sections with periods" do
     before do
       FactoryGirl.create_list(:course_with_sections_with_periods, 4)
