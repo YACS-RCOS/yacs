a55c84812680e1eb48d2ef9a114918b1ad033a5e
Richard Young <richiejoeyoung@gmail.com>
2016-07-18 00:33:30 -0400

    adjust section styleing, crn positioning, fix #courses?section_id=:id,:id

--- app/controllers/api/v5/courses_controller.rb
+++ app/controllers/api/v5/courses_controller.rb
@@ -8,7 +8,6 @@ class Api::V5::CoursesController < Api::V5::ApiController
     else
       filter_model Course
       filter :section_id do |q|
-        # q.joins(:sections).where "sections.id = ANY (ARRAY[#{params[:section_id]}])"
         q.joins(:sections).where :"sections.id" => any(:section_id)
       end
       filter :department_code do |q|
