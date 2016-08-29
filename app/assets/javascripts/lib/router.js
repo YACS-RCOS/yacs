bc46fcb0dccb5e5aba10adc6dc06485001162de2
Richie <richiejoeyoung@gmail.com>
2016-08-28 21:32:45 -0400

    [WIP] adds router

---
+++ app/assets/javascripts/lib/router.js
@@ -0,0 +1,30 @@
+Yacs.router = new function () {
+  var self = this;
+
+  var routes = {};
+
+  var queryToHash = function (queryString) {
+    var paramsHash = {};
+    queryString = queryString.replace(/^(\?|\&)+|\&+$/g, '');
+    queryString.split('&').forEach(function (entry) {
+      entry = entry.split('=');
+      if (entry[0].length) paramsHash[entry[0]] = entry[1];
+    });
+    return paramsHash;
+  };
+
+  self.define = function (path, callback) {
+    routes[path] = callback;
+  };
+
+  self.visit = function (path, params) {
+    routes[path](params);
+  };
+
+  window.addEventListener('hashchange', function () {
+    var resource = window.location.hash.slice(1).split('?');
+    var path = resource[0].length ? resource[0] : '/';
+    var params = resource[1] || '';
+    if (routes[path]) routes[path](queryToHash(query));
+  }, false);
+};
