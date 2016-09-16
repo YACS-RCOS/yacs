052e7afdaf35204d7b5311fc852ecc8706be918d
Richie <richiejoeyoung@gmail.com>
2016-09-16 03:01:28 -0400

    use npm to install phantomjs

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ mkdir -p ./tmp/pids/puma
 
 docker-compose down
 docker-compose build
-docker-compose run web rake db:create db:migrate
-docker-compose run web rake assets:precompile
+docker-compose run web bundle exec rake db:create db:migrate
+docker-compose run web bundle exec rake assets:precompile
 docker-compose up -d
