36c7994ab9d9d93dea921db605dc9d5508dde112
Richie <richiejoeyoung@gmail.com>
2016-09-18 16:36:07 -0400

    remoces docker-compose.common.yml

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ mkdir -p ./tmp/pids/puma
 
 docker-compose down
 docker-compose build
+docker-compose run web rm public/index.html
 docker-compose run web bundle exec rake db:create db:migrate
 docker-compose run web bundle exec rake assets:precompile
 docker-compose up -d
