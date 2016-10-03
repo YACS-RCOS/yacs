3ae030bbab688b2bf7b399d18ecac83c8aba504d
Ryan Stillings <rystills@gmail.com>
2016-10-03 01:52:26 -0400

    Replace wildcard character with modified schema
    Added query analyzer for partial matching to solr schema, eliminating
    the need for automatically inserting wildcard character into searches,
    and fixing a bug where exact matching would not work correctly.

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ActiveRecord::Base
   validates  :number, presence: true, uniqueness: { scope: :department_id }
   default_scope { order(number: :asc) }
   
-  searchable do #searchable block required by sunspot
+  searchable do #searchable block required by sunspot (specifies which fields to index and search on)
     text :number, :name, :description
   end
 
@@ -12,20 +12,18 @@ class Course < ActiveRecord::Base
     joins(:department).where("departments.code = ? AND number = ?", code, number).first
   end
 
-  #Returns the result of a string search using SQL Query
+  #Returns the result of a string search using sunspot_solr (formerly using SQL Query)
   #params: a list of string search terms (split by whitespace from user search text)
   def self.search params
-
-  params.map! {|p| p+"*"}
-  courses = Sunspot.search(Course) do
-    fulltext params do
-	  fields(:description, :number => 10, :name => 5.0) #weight results most strongly by number, then name, and finally description
-	end
-	paginate :page => 1, :per_page => 25
-  end.results
+    courses = Sunspot.search(Course) do
+      fulltext params do
+	    fields(:description, :number => 10, :name => 5.0) #weight results most strongly by number, then name, and finally description
+	    phrase_fields :name => 3.0 #weight titles that contain a phrase of search terms more strongly
+	  end
+	  paginate :page => 1, :per_page => 25 #place a maximum of 25 results on each page
+    end.results #store search results in courses in one step
 
 =begin
-search_params = params.join(' & ')
     search_params = params.join(' & ')
     query = <<-SQL
       SELECT * FROM (
