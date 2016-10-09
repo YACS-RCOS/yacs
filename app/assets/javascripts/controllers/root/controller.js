223600ac4f9b624a406e5f1d6a4a185e2565385e
Richie <richiejoeyoung@gmail.com>
2016-10-09 14:46:23 -0400

    fix merge conflcits in root controller

--- app/assets/javascripts/controllers/root/controller.js
+++ app/assets/javascripts/controllers/root/controller.js
@@ -1,4 +1,5 @@
 // TODO: Move index.html to hbs template
+// TODO: emulate text input and caret
 
 /**
  * Root view of YACS. Controls site navigation and search
@@ -30,39 +31,16 @@ Yacs.views.root = function (target) {
     var key = event.keyCode;
     if (!(event.ctrlKey || event.metaKey)) {
       if ((key >= 48 && key <= 105) || key == 32) {
-<<<<<<< HEAD:app/assets/javascripts/controllers/root/controller.js
-        if (key == 127 && searchbar.value.length <= 1)
-          Yacs.views.departments(target);
-=======
         // normal keys focus the searchbar
->>>>>>> 463b014b8cd18afab99a1a8660f34a06d7fe2fce:app/assets/javascripts/views/index.js
         searchbar.focus();
       } else if (key == 13) {
         // enter searches
         if (searchbar.value) {
-<<<<<<< HEAD:app/assets/javascripts/controllers/root/controller.js
           Yacs.router.visit('/courses?search=' + searchbar.value);
-        } else {
-          Yacs.router.visit('/');
         }
-      } else if ((key == 8 || key == 46) && searchbar.value.length <= 1) {
-        Yacs.router.visit('/');
-=======
-          Yacs.models.courses.query({ search: searchbar.value,
-                                      show_sections: true,
-                                      show_periods: true },
-            function (data, success) {
-              if (success)
-                Yacs.views.courses(content, data);
-          });
-        }
->>>>>>> 463b014b8cd18afab99a1a8660f34a06d7fe2fce:app/assets/javascripts/views/index.js
       }
     }
   });
-
-  // Yacs.on('click', document.body, function () { searchbar.focus() });
-  // TODO lol previous line was a bit of an oversight, must emulate caret
 
   searchbar.focus();
 
