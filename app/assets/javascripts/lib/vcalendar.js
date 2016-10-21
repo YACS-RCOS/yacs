cdbc280093c25ff94d8e776d91479c6876e8db29
copperwater <aosdict@gmail.com>
2016-10-21 13:27:01 -0400

    Made vcalendar a full lib file

--- app/assets/javascripts/lib/vcalendar.js
+++ app/assets/javascripts/lib/vcalendar.js
@@ -1,60 +1,88 @@
 /**
- * Create an vCalendar file with all the current courses on it,
- * set to weekly, starting with the current week.
+ * @namespace vCalendar
+ * @description Functions associated with the vCalendar (ICS) download on the schedule page
+ * @memberOf Yacs
  */
-var createVCalendar = function(periods) {
-  var vCalendarData = 'BEGIN:VCALENDAR\r\n'+
-    'VERSION:2.0\r\n' +
-    'PRODID:-//yacs//NONSGML v1.0//EN\r\n';
+window.Yacs.vCalendar = new function() {
+  var self = this;
 
-  var uidCounter = 0;
+  /**
+   * Given a set of periods in the same form as they would be used
+   * to generate a single schedule, return a string containing
+   * that data formatted into a VCalendar, which if it were a file
+   * could be imported into a calendar application.
+   * All periods are set to recur weekly, starting at the current
+   * week.
+   */
+  self.createVCalendar = function(periods) {
+    var vCalendarData = 'BEGIN:VCALENDAR\r\n'+
+      'VERSION:2.0\r\n' +
+      'PRODID:-//yacs//NONSGML v1.0//EN\r\n';
 
-  // Helper to pad single digit numbers with a 0 and leave double digit numbers unchanged.
-  var pad0 = function(num) { return ('0' + num).slice(-2); }
+    var uidCounter = 0;
 
-  // Helper to extract the vCalendar formatted time from a Date. It looks like this:
-  // YYYYMMDDTHHMMSS, where T is a literal T
-  var getVCalendarStamp = function(d) {
-    return d.getFullYear() + pad0(d.getMonth()+1) + pad0(d.getDate()) + 'T'
-      + pad0(d.getHours()) + pad0(d.getMinutes()) + '00';
-  }
+    // Helper to pad single digit numbers with a 0 and leave double digit numbers unchanged.
+    var pad0 = function(num) { return ('0' + num).slice(-2); }
 
-  // Helper to convert a single period into a full VEVENT.
-  var periodToVevent = function(period) {
-    var d = new Date();
-    var nowstamp = getVCalendarStamp(d); // save for the DTSTAMP field
+    // Helper to extract the vCalendar formatted time from a Date. It looks like this:
+    // YYYYMMDDTHHMMSS, where T is a literal T
+    var getVCalendarStamp = function(d) {
+      return d.getFullYear() + pad0(d.getMonth()+1) + pad0(d.getDate()) + 'T'
+        + pad0(d.getHours()) + pad0(d.getMinutes()) + '00';
+    }
 
-    // dates need to be shuffled around by setting the date, so calculate how much it
-    // needs to move by
-    var weekday_offset = period.day - d.getDay();
-    d.setDate(d.getDate() + weekday_offset);
+    // Helper to convert a single period into a full VEVENT.
+    var periodToVevent = function(period) {
+      var d = new Date();
+      var nowstamp = getVCalendarStamp(d); // save for the DTSTAMP field
 
-    // at this point the date is set correctly, then the time needs to be set
-    d.setHours(Math.floor(period.start / 60));
-    d.setMinutes(period.start % 60);
+      // dates need to be shuffled around by setting the date, so calculate how much it
+      // needs to move by
+      var weekday_offset = period.day - d.getDay();
+      d.setDate(d.getDate() + weekday_offset);
 
-    var startstamp = getVCalendarStamp(d);
+      // at this point the date is set correctly, then the time needs to be set
+      d.setHours(Math.floor(period.start / 60));
+      d.setMinutes(period.start % 60);
 
-    // compute end and take end stamp
-    d.setMinutes(d.getMinutes() + (period.end - period.start));
-    var endstamp = getVCalendarStamp(d);
+      var startstamp = getVCalendarStamp(d);
 
-    uidCounter++;
-    return 'BEGIN:VEVENT\r\n' +
-      'UID:event' + uidCounter + '@yacs.cs.rpi.edu\r\n' +
-      'SUMMARY:' + period.tooltip + '\r\n' +
-      'DTSTAMP:' + nowstamp + '\r\n' +
-      'DTSTART:' + startstamp + '\r\n' +
-      'DTEND:' + endstamp + '\r\n' +
-      'RRULE:FREQ=WEEKLY' + '\r\n' +
-      'END:VEVENT\r\n';
+      // compute end and take end stamp
+      d.setMinutes(d.getMinutes() + (period.end - period.start));
+      var endstamp = getVCalendarStamp(d);
+
+      uidCounter++;
+      return 'BEGIN:VEVENT\r\n' +
+        'UID:event' + uidCounter + '@yacs.cs.rpi.edu\r\n' +
+        'SUMMARY:' + period.tooltip + '\r\n' +
+        'DTSTAMP:' + nowstamp + '\r\n' +
+        'DTSTART:' + startstamp + '\r\n' +
+        'DTEND:' + endstamp + '\r\n' +
+        'RRULE:FREQ=WEEKLY' + '\r\n' +
+        'END:VEVENT\r\n';
+
+    };
+
+    each(periods, function(period) {
+      vCalendarData += periodToVevent(period);
+    });
+    vCalendarData += 'END:VCALENDAR';
+    return vCalendarData;
 
   };
 
-  each(periods, function(period) {
-    vCalendarData += periodToVevent(period);
-  });
-  vCalendarData += 'END:VCALENDAR';
-  return vCalendarData;
-
+  /**
+   * Prompt the user to download a vCalendar string as a file
+   * named yacs-schedule.ics.
+   * Do this by attaching a phantom <a> to the DOM and setting its
+   * href to the calendar data, then simulating a click on it.
+   */
+  self.download = function(vCalendarData) {
+    var elt = document.createElement('a');
+    elt.setAttribute('href', 'data:text/calendar;charset=utf8,' + encodeURIComponent(vCalendarData));
+    elt.setAttribute('download', 'yacs-schedule.ics');
+    document.body.appendChild(elt);
+    elt.click();
+    document.body.removeChild(elt);
+  };
 };
