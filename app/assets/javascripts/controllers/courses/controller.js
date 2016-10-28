6d64210136d0730628e90066c284f53993753474
copperwater <aosdict@gmail.com>
2016-10-28 13:31:32 -0400

    Implemented new conflicts algorithm

--- app/assets/javascripts/controllers/courses/controller.js
+++ app/assets/javascripts/controllers/courses/controller.js
@@ -54,11 +54,120 @@ Yacs.views.courses = function (target, params) {
     }
   }
 
-  /** Given a section ID, the current selections, (and data in the conflicts cache),
+  /**
+   * Flatten the selection data into an array of arrays [sectionId, courseId]
+   * sorted by section id. Also generate a map of course ids to the number of
+   * sections that are selected by that course.
+   * This is a preprocessing step for evaluating conflicts on each section
+   * efficiently (in O(k + s) time) without having to refer back to the selections
+   * in their normal format.
+   * Return an object containing the flattened array and the course id map.
+   */
+  var flattenSelections = function(selections) {
+    selectionsFlat = []
+    courseSelectionCounts = {}
+
+    for(var cid in selections) {
+      courseSelectionCounts[cid] = 0;
+      var sidlen = selections[cid].length;
+
+      for(var i=0; i<sidlen; ++i) {
+        var sid = selections[cid][i];
+        selectionsFlat.push([sid, cid]);
+        courseSelectionCounts[cid]++;
+      }
+
+    }
+    // then sort selectionsFlat by ascending order of sid
+    // this could be improved with an optimized k-merge of the sectionId arrays, TODO
+    selectionsFlat.sort(function(sidcid1, sidcid2) {
+      // the arguments are those 2-element arrays [sid, cid] from above
+      return sidcid1[0] - sidcid2[0];
+    });
+    return {
+      'selectionsFlat': selectionsFlat,
+      'courseSelectionCounts': courseSelectionCounts,
+    };
+  };
+
+  /** Given a section ID, the course ID of the course the section is associated with,
+   * the values returned from flattenSelections, (and data in the conflicts cache),
    * return a boolean of whether this section has any conflicts with current selections.
    */
-  var doesConflict = function(selections, sectid) {
+  var doesConflict = function(sectId, courseId, selectionsFlat, courseSelectionCounts) {
+    if(! (courseId in Yacs.cache.conflicts)) {
+      // can't do anything, not going to ask the API for information
+      return false;
+    }
+    // ASSUMPTIONS:
+    // Section IDs in the flattened selections object and conflicts list are in increasing numeric order.
+    // At least one of the arrays contains integer section IDs. This breaks if both are strings.
 
+    var conflicts = Yacs.cache.conflicts[sectId];
+    var i=0, j=0;
+    var imax = conflicts.length, jmax = selectionsFlat.length;
+    while(i < imax && j < jmax) {
+      // remember, selectionsFlat contains Array[2]s containing a section id and a course id
+      var selectedSectionId = selectionsFlat[j][0];
+      var selectedCourseId = selectionsFlat[j][1];
+      if(conflicts[i] < selectedSectionId)
+        ++i;
+      else if(conflicts[i] > selectedSectionId)
+        ++j;
+      else if(conflicts[i] == selectedSectionId) {
+        --courseSelectionCounts[selectedCourseId];
+        if(courseSelectionCounts[selectedCourseId] < 1) {
+          // all sections for a single selected course conflict with this section
+          // the function does not need to go any further
+          return true;
+        }
+        ++i;
+        ++j;
+      }
+    }
+    // if here, either the conflict array or the flat selections array has been exhausted
+    return false;
+
+    /*
+    for(var cid in selections) {
+      var i=0, j=0;
+      var imax = selections[cid].length, jmax = conflicts.length;
+      var available_sections = imax;
+      while(i<imax && j < jmax) {
+        if(selections[cid][i] < conflicts[j]) {
+          ++i;
+        }
+        else if(selections[cid][i] > conflicts[j]) {
+          ++j;
+        }
+        else if(selections[cid][i] == conflicts[j]) {
+          ++i;
+          ++j;
+          --available_sections;
+        }
+        else {
+          // nan or some other bad comparison
+          // will infinite loop
+        }
+      }
+      if(available_sections < 1) {
+        // every selected section for this course conflicts with this section
+        // therefore this section "conflicts with selected courses"
+        return true;
+      }
+    }
+    */
+  };
+
+  /**
+   * Make a deep copy of the courseSelectionCounts object and return it.
+   */
+  var copyCounts = function(courseSelectionCounts) {
+    var output = {};
+    for(var key in courseSelectionCounts) {
+      output[key] = courseSelectionCounts[key];
+    }
+    return output;
   };
 
   /**
@@ -68,24 +177,29 @@ Yacs.views.courses = function (target, params) {
   var updateSelected = function () {
     // It would be a good idea for conflicts to optimize by checking only the course that
     // actually updated. Actually, why doesn't this already do that?
-    /*
     var selected = Yacs.user.getSelections();
+    flatObj = flattenSelections(selected);
+
     each(target.querySelectorAll('course'), function (course) {
       var courseSelected = false;
       var sections = course.querySelectorAll('section');
       if (sections.length > 0) {
         courseSelected = true;
         each(sections, function (section) {
-          var sectionSelected = selected.indexOf(section.dataset.id) !== -1;
-          var hasConflict = doesConflict(section.dataset.id, selected);
+          // TODO optimize this next line somehow
+          var sectionSelected = Yacs.user.hasSelection(section.dataset.id, course.dataset.id);
           section.classList.toggle('selected', sectionSelected);
+
+          // doesConflict modifies the data courseSelectionCounts passed into it, so do a deep copy of it
+          var hasConflict = doesConflict(section.dataset.id, selected, flatObj.selectionsFlat, copyCounts(flatObj.courseSelectionCounts));
+          section.classList.toggle('conflicts', hasConflict);
+
           if (!sectionSelected && !section.classList.contains('closed'))
             courseSelected = false;
         });
       }
       course.classList.toggle('selected', courseSelected);
     });
-    */
   };
 
   Yacs.models.courses.query(params, function (data, success) {
