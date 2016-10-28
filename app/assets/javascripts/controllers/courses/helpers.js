2d5e5f8d370234f1b224df708a6e6740d6f7b35a
copperwater <aosdict@gmail.com>
2016-10-27 22:49:06 -0400

    Removed all helper conflicts logic

--- app/assets/javascripts/controllers/courses/helpers.js
+++ app/assets/javascripts/controllers/courses/helpers.js
@@ -27,65 +27,6 @@ Handlebars.registerHelper('closed_status', function (s) {
   return new Handlebars.SafeString(s.seats > 0 && s.seats_taken >= s.seats ? 'closed' : '');
 });
 
-/* Determine whether a section's conflicts list contains any section ids in common with
- * currently selections. Set some variables on this (the current section object)
- * for subsequent Handlebars calls to use.
- * The advantage of this method is that the expensive comparison of section
- * conflicts and selected section ids is only performed once per section.
- */
-Handlebars.registerHelper('evaluateConflict', function(block) {
-  // Populate the conflicts cache here, with whatever data is available
-  // If something already exists in the conflicts cache, don't update - it most likely is unchanged
-  if(! (this.id in Yacs.cache.conflicts)) {
-    Yacs.cache.conflicts[this.id] = this.conflicts;
-  }
-  var cachedConflicts = Yacs.cache.conflicts[this.id];
-
-  // Don't want to call getSelections once for every section, use
-  // the tmp cache for this
-  if(! ('selections' in Yacs.cache.tmp)) {
-    // Selections MUST be taken as ints. Otherwise it will try
-    // a string comparison and fail on things like "7" > "600"
-    Yacs.cache.tmp.selections = Yacs.user.getSelectionsAsInts();
-  }
-  var selectionsAsInts = Yacs.cache.tmp.selections;
-
-  // Iterate over the selections list and the conflicts list
-  // to determine whether there is a section id in common
-  // and therefore a conflict.
-  // ASSUMES: the selections list and conflicts list
-  // are both sorted in ascending order
-  var hasConflict = false;
-  var i=0, j=0;
-
-  while(i<cachedConflicts.length && j < selectionsAsInts.length) {
-    if(cachedConflicts[i] < selectionsAsInts[j]) {
-      ++i;
-    }
-    else if(cachedConflicts[i] > selectionsAsInts[j]) {
-      ++j;
-    }
-    else if(cachedConflicts[i] == selectionsAsInts[j]) { // ==
-      hasConflict = true;
-      break;
-    }
-    else {
-      // something is NaN or whatever went wrong
-      break;
-    }
-  }
-
-  if(hasConflict) {
-    this.conflictClass = 'conflicts';
-    this.hasConflict = true;
-  }
-  else {
-    this.conflictClass = '';
-    this.hasConflict = false;
-  }
-  return block.fn(this);
-});
-
 Handlebars.registerHelper('day_name', function (n) {
   return new Handlebars.SafeString(['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'][n]);
 });
