f80b1f8a0e906d45497fbc454b59d764211e0d83
copperwater <aosdict@gmail.com>
2016-11-02 18:53:38 -0400

    Added new step to click section by id

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -3,6 +3,11 @@ When(/^I click the (.*) "(.*)"$/) do |elem, text|
   find(elem, text: text).click
 end
 
+When(/^I click the (.+) with id (\d+)$/) do |elem, id|
+  css = "#{elem}[data-id='#{id}']"
+  page.find(css).click
+end
+
 When(/^I click "(.*)" in the header bar$/) do |text|
   find(".sidelink", text: text).click
 end
