3b47ea5f31e11020ff3dc7351a96c4b46eb21965
Mark Robinson <robinm8@keybase.io>
2016-11-01 20:34:03 -0400

    Docker NGINX HTTPS Support

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -9,6 +9,11 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RAILS_ENV'] || 'development'
+
+ssl_bind '0.0.0.0', '3001', {
+  key: "/etc/nginx/ssl/yacs.key",
+  cert: "/etc/nginx/ssl/yacs.cer"
+}
 
 pidfile "/var/run/puma/puma.pid"
 state_path "/var/run/puma/puma.state"
