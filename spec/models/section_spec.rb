477f503d39c79223e83a10dcbcfc38a044ceb2f8
YushYush <yush.mishra@gmail.com>
2016-12-12 20:39:00 -0500

    creates test for period sorting when period info is updated

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,12 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Section do
-	it 'sorts periods by day' do 
+	context 'period day, start time, or end time updated' do
+		before do
+			@section = create(:section, num_periods: 5, 
+				periods_day: [3, 5, 2, 2, 4], 
+				periods_start: [1200, 800, 1600, 800, 800], 
+				periods_end: [1400, 900, 1800, 900, 1000], 
+				periods_type: ['LAB', 'LEC', 'TEST', 'LEC', 'LEC'])
+		end
+
+		it 'sorts periods by day, and then start time' do 
+			expect(@section.periods_day).to eq [2, 2, 3, 4, 5]
+			expect(@section.periods_start).to eq [800, 1600, 1200, 800, 800]
+			expect(@section.periods_end).to eq [900, 1800, 1400, 1000, 900]
+			expect(@section.periods_type).to eq ['LEC', 'TEST', 'LAB', 'LEC', 'LEC']
+		end
 	end
 
-	it 'sorts periods on the same day by start time' do 
-	end
+	context 'all updates excluding period day, start time, and end time' do
 
-	it 'sorts periods on the same day and start time by end time' do 
 	end
 end
