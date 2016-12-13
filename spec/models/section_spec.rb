00f7495fa2c59fa294cb500a455e03b988a2bb3b
YushYush <yush.mishra@gmail.com>
2016-12-12 19:20:25 -0500

    creates spec file for periods

---
+++ spec/models/section_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Section do
+	it 'sorts periods by day' do 
+	end
+
+	it 'sorts periods on the same day by start time' do 
+	end
+
+	it 'sorts periods on the same day and start time by end time' do 
+	end
+end
