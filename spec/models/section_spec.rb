459284ff5ebc105646a7102e0852638784a7da3b
YushYush <yush.mishra@gmail.com>
2016-12-12 20:52:17 -0500

    creates test to not sort without sort callback

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Section do
 		end
 
 		it 'sorts periods by day, and then start time' do 
+			expect(@section.num_periods).to eq 5
 			expect(@section.periods_day).to eq [2, 2, 3, 4, 5]
 			expect(@section.periods_start).to eq [800, 1600, 1200, 800, 800]
 			expect(@section.periods_end).to eq [900, 1800, 1400, 1000, 900]
@@ -19,6 +20,22 @@ RSpec.describe Section do
 	end
 
 	context 'all updates excluding period day, start time, and end time' do
+		before do
+			Section.skip_callback(:sort_periods)
+			@section = create(:section, num_periods: 5, 
+				periods_day: [3, 5, 2, 2, 4], 
+				periods_start: [1200, 800, 1600, 800, 800], 
+				periods_end: [1400, 900, 1800, 900, 1000], 
+				periods_type: ['LAB', 'LEC', 'TEST', 'LEC', 'LEC'])
+			Section.set_callback(:sort_periods)
+		end
 
+		if 'does not sort period' do 
+			expect(@section.num_periods).to eq 5
+			expect(@section.periods_day).to eq [3, 5, 2, 2, 4]
+			expect(@section.periods_start).to eq [1200, 800, 1600, 800, 800]
+			expect(@section.periods_end).to eq [1400, 900, 1800, 900, 1000]
+			expect(@section.periods_type).to eq ['LAB', 'LEC', 'TEST', 'LEC', 'LEC']
+		end
 	end
 end
