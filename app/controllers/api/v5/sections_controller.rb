3953c43646ff31a3b3342461a1231ea2fd94a109
kburk1997 <kburk97@optonline.net>
2017-02-21 15:59:25 -0500

    add new and create methods to section controller

--- app/controllers/api/v5/sections_controller.rb
+++ app/controllers/api/v5/sections_controller.rb
@@ -3,4 +3,13 @@ class Api::V5::SectionsController < Api::V5::ApiController
     filter_model Section
     filter_any :id, :course_id, :name, :crn
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+  
 end
