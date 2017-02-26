008d2484e47e0eabb8e41318a7ed2e230852c9e6
rvm2113 <rutvikmanohar@gmail.com>
2017-02-26 16:42:50 -0500

    Completed tests for seats and seats_taken parameters

--- app/controllers/api/v5/sections_controller.rb
+++ app/controllers/api/v5/sections_controller.rb
@@ -5,7 +5,15 @@ class Api::V5::SectionsController < Api::V5::ApiController
   end
   def update
   	# # puts params
-  	Section.find(params[:id]).update(seats_taken: params[:seats_taken])
+  	#Section.find(params[:id]).update(seats_taken: params[:seats_taken])
+    Section.find(params[:id]).tap{ |section| section.update!(section_params)}
   	render :action => :index
   end
+  private 
+
+    def section_params
+      #binding.pry
+      params.require(:section).permit(:name, :crn, :seats, :seats_taken, 
+        :instructors, :num_periods, :course_id)
+    end 
 end
