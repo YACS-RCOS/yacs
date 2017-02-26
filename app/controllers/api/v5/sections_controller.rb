4a4bba6d7eab8561d3c9087a781379f660e67377
Kathleen Burkhardt <kburk97@optonline.net>
2017-02-26 16:49:59 -0500

    Update sections_controller.rb

--- app/controllers/api/v5/sections_controller.rb
+++ app/controllers/api/v5/sections_controller.rb
@@ -3,13 +3,4 @@ class Api::V5::SectionsController < Api::V5::ApiController
     filter_model Section
     filter_any :id, :course_id, :name, :crn
   end
-
-  def new
-
-  end
-
-  def create
-
-  end
-  
 end
