3bdc8ba1b31421390e04c96d64e93d2e6e48f5a4
copperwater <aosdict@gmail.com>
2017-02-28 21:35:21 -0500

    Changed several minor things to pass ESLint checks

--- vendor/assets/javascripts/schedule/schedule.js
+++ vendor/assets/javascripts/schedule/schedule.js
@@ -1,23 +1,25 @@
-window.Schedule = function (scheduleContainer, options) {
+'use strict';
+
+window.Schedule = function (scheduleContainer, givenOptions) {
   var self = this;
 
-  var NUM_COLORS        = 7;
+  /*                       PINK       BLUE       YELLOW     GREEN      PURPLE     BROWN      ORANGE */
+  /* var SELECTED_COLORS = ['#ff3575', '#19b5f2', '#ffcf56', '#59dc68', '#d57fdd', '#ac8f71', '#ff9c46']; */
   var TEXT_COLORS       = ['#d1265d', '#1577aa', '#bf8a2e', '#008a2e', '#853d80', '#9d5733', '#d9652b'];
   var BACKGROUND_COLORS = ['#ffd4df', '#ceeffc', '#fff4d0', '#dcf7da', '#f7e2f7', '#ede6df', '#ffe9cf'];
   var BORDER_COLORS     = ['#ff2066', '#00aff2', '#ffcb45', '#48da58', '#d373da', '#a48363', '#ff9332'];
-  var SELECTED_COLORS   = ['#ff3575', '#19b5f2', '#ffcf56', '#59dc68', '#d57fdd', '#ac8f71', '#ff9c46'];
-  // /*                         PINK       BLUE       YELLOW     GREEN      PURPLE     BROWN      ORANGE */
+  var NUM_COLORS = Math.min(TEXT_COLORS.length, BACKGROUND_COLORS.length, BORDER_COLORS.length);
 
-  // var NUM_COLORS        = 6;
+
   // var TEXT_COLORS       = ['#720', '#722', '#661', '#227', '#166', '#616'];
   // var BACKGROUND_COLORS = ['#fdc', '#edd', '#eed', '#dde', '#dee', '#ede'];
   // var BORDER_COLORS     = ['#b62', '#b44', '#994', '#448', '#499', '#949'];
 
-  options = options || {};
-  options.daySpan   = options.daySpan   || 5;
-  options.dayBegin  = options.dayBegin  || 1;
-  options.gridSize  = options.gridSize  || 60;
-  options.timeSpan  = Math.ceil((options.timeSpan  || 720) / 60) * 60;
+  var options = givenOptions || {};
+  options.daySpan = options.daySpan || 5;
+  options.dayBegin = options.dayBegin || 1;
+  options.gridSize = options.gridSize || 60;
+  options.timeSpan = Math.ceil((options.timeSpan || 720) / 60) * 60;
   options.timeBegin = Math.ceil((options.timeBegin || 480) / 60) * 60;
 
   var scheduleElement = document.createElement('schedule-view');
@@ -28,12 +30,21 @@ window.Schedule = function (scheduleContainer, options) {
   scheduleElement.appendChild(gridElement);
   scheduleContainer.appendChild(scheduleElement);
 
-  var daySize = function (n) {
-    return n * (100 / options.daySpan) + '%';
+  /**
+   * Returns the left offset of the given day, as a percentage.
+   */
+  var dayOffset = function (n) {
+    return (n * (100 / options.daySpan)) + '%';
   };
 
-  var timeSize = function (n) {
-    return n * (100 / options.timeSpan) + '%';
+  // The width of a single day, as a percentage. This is fixed based on the number of days.
+  var dayWidth = (100 / options.daySpan) + '%';
+
+  /**
+   * Returns the offset of the given time, as a percentage.
+   */
+  var timeOffset = function (n) {
+    return (n * (100 / options.timeSpan)) + '%';
   };
 
   self.addEvent = function (event) {
@@ -42,13 +53,13 @@ window.Schedule = function (scheduleContainer, options) {
     var eventBackground = document.createElement('event-background');
     var colorIndex      = event.colorNum % NUM_COLORS;
 
-    eventText.innerHTML                   = event.title.join('<br>');
-    eventText.style.color                 = TEXT_COLORS[colorIndex];
-    eventElement.style.top                = timeSize(event.start - options.timeBegin);
-    eventElement.style.left               = daySize(event.day - options.dayBegin);
-    eventElement.style.width              = 'calc(' + daySize(1) + ' - 6px)';
-    eventElement.style.height             = 'calc(' + timeSize(event.end - event.start) + ' - 2px)';
-    eventElement.style.borderColor        = BORDER_COLORS[colorIndex];
+    eventText.innerHTML = event.title.join('<br>');
+    eventText.style.color = TEXT_COLORS[colorIndex];
+    eventElement.style.top = timeOffset(event.start - options.timeBegin);
+    eventElement.style.left = dayOffset(event.day - options.dayBegin);
+    eventElement.style.width = 'calc(' + dayWidth + ' - 6px)';
+    eventElement.style.height = 'calc(' + timeOffset(event.end - event.start) + ' - 2px)';
+    eventElement.style.borderColor = BORDER_COLORS[colorIndex];
     eventBackground.style.backgroundColor = BACKGROUND_COLORS[colorIndex];
 
     if ('tooltip' in event) {
@@ -62,8 +73,9 @@ window.Schedule = function (scheduleContainer, options) {
 
   self.clearEvents = function () {
     var events = scheduleElement.querySelectorAll('schedule-event');
-    for (var e = 0; e < events.length; ++e)
+    for (var e = 0; e < events.length; ++e) {
       events[e].parentNode.removeChild(events[e]);
+    }
   };
 
   self.setEvents = function (events) {
@@ -80,15 +92,23 @@ window.Schedule = function (scheduleContainer, options) {
   var drawLegend = function () {
     for (var r = 1; r < options.timeSpan / 60; ++r) {
       var hourElement = document.createElement('legend-hour');
-      var hour = options.timeBegin / 60 + r;
+      var hour = (options.timeBegin / 60) + r;
 
-      if      (hour == 0)  hour = '12 AM';
-      else if (hour == 12) hour = 'Noon';
-      else if (hour < 12)  hour = hour + ' AM';
-      else                 hour = hour - 12 + ' PM';
+      if (hour === 0) {
+        hour = '12 AM';
+      }
+      else if (hour < 12) {
+        hour = hour + ' AM';
+      }
+      else if (hour === 12) {
+        hour = 'Noon';
+      }
+      else {
+        hour = (hour - 12) + ' PM';
+      }
 
       hourElement.textContent = hour;
-      hourElement.style.top = 'calc(' + timeSize(60 * r) + ' - 0.75em)';
+      hourElement.style.top = 'calc(' + timeOffset(60 * r) + ' - 0.75em)';
       legendElement.appendChild(hourElement);
     }
   };
@@ -96,11 +116,11 @@ window.Schedule = function (scheduleContainer, options) {
   var drawGrid = function () {
     for (var d = 0; d < options.daySpan; ++d) {
       var dayElement = document.createElement('grid-day');
-      dayElement.style.width = daySize(1);
+      dayElement.style.width = dayWidth;
 
       for (var r = 0; r < options.timeSpan / options.gridSize; ++r) {
         var hourElement = document.createElement('grid-hour');
-        hourElement.style.height = timeSize(options.gridSize);
+        hourElement.style.height = timeOffset(options.gridSize);
         dayElement.appendChild(hourElement);
       }
 
