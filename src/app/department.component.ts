6b8a79af8040af091d2e4c4a23b38e44ed81b73e
copperwater <aosdict@gmail.com>
2017-04-03 12:39:02 -0400

    Added departments component

---
+++ src/app/department.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+export class Department {
+    id: number;
+    code: string;
+    name: string;
+}
+
+@Component({
+    selector: 'department',
+    templateUrl: './department.component.html',
+})
+export class DepartmentComponent {
+  // declare "dept" as a field of this component that comes from a []=
+  // expression field in another component (schools, in this case)
+  @Input() dept: Department;
+}
