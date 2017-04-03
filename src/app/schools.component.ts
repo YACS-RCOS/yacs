7a745a16ad7d6b40825f5077b396046e4a9c5f1a
copperwater <aosdict@gmail.com>
2017-04-03 12:39:39 -0400

    Flesh out schools with some test data and add departments as a sub component

--- src/app/schools.component.ts
+++ src/app/schools.component.ts
@@ -1,4 +1,30 @@
 import { Component } from '@angular/core';
+import { Department } from './department.component';
+
+export class School {
+    id: number;
+    name: string;
+    departments: Department[];
+}
+
+const SCHOOL_TEST_DATA: School[] = [
+    {
+        id:1,
+        name:'Science',
+        departments: [
+            {
+                id: 1,
+                code: 'CSCI',
+                name: 'Computer Science'
+            },
+            {
+                id: 2,
+                code: 'MATH',
+                name: 'Mathematics'
+            }
+        ]
+    }
+];
 
 @Component({
   selector: 'schools',
@@ -6,6 +32,6 @@ import { Component } from '@angular/core';
   // styleUrls: []
 })
 export class SchoolsComponent {
-    // schools: School[];
+    schools = SCHOOL_TEST_DATA;
 }
 
--- src/app/schools.module.ts
+++ src/app/schools.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+// this is added so that an ngFor in the schools html will work.
+import { CommonModule } from '@angular/common';
 
 import { SchoolsComponent } from './schools.component';
+import { DepartmentComponent } from './department.component';
 
 @NgModule({
     declarations: [
-        SchoolsComponent
+        SchoolsComponent,
+        DepartmentComponent
     ],
-    imports: [],
+    imports: [
+      CommonModule
+    ],
     providers: [],
 })
 export class SchoolsModule {}
