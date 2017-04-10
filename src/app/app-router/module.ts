91013a9927b41b9dc39cbd20a6bb63ebe6a8c059
copperwater <aosdict@gmail.com>
2017-04-09 21:47:01 -0400

    Added /schedules to the router

--- src/app/app-router/module.ts
+++ src/app/app-router/module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SchoolListComponent } from '../school-list/component';
 import { CourseListComponent } from '../course-list/component';
+import { ScheduleViewComponent } from '../schedule-view/component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/schools', pathMatch: 'full' },
-    { path: 'schools', component: SchoolListComponent },
-    { path: 'courses', component: CourseListComponent },
+  { path: '', redirectTo: '/schools', pathMatch: 'full' },
+  { path: 'schools', component: SchoolListComponent },
+  { path: 'courses', component: CourseListComponent },
+  { path: 'schedules', component: ScheduleViewComponent },
 ];
 
 @NgModule({
