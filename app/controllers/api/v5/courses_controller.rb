f7990842373828e0b32191fed346248811e8a62b
Perri Adams <perrihadams@gmail.com>
2017-06-12 21:22:38 -0400

    .

--- app/controllers/api/v5/courses_controller.rb
+++ app/controllers/api/v5/courses_controller.rb
@@ -1,6 +1,4 @@
 class Api::V5::CoursesController < Api::V5::ApiController
-   include ActionController::Live
-   
    caches_action :index, if: Proc.new { |c| c.params[:department_id].present? && @show_sections && @show_periods },
      cache_path: Proc.new { |c| "/api/v5/courses.json?department_id=#{c.params[:department_id]}" } # TODO: rework caching scheme
  
