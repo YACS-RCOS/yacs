ca91ad8b77975cd4ee4e736f39cd9414583f4968
perribus <perrihadams@gmail.com>
2017-06-15 17:37:05 -0400

    Create event_sender.rb

--- lib/event_sender.rb
+++ lib/event_sender.rb
@@ -1,19 +1,41 @@
 class EventSender
-	def self.send_event(obj, fieldname, before=0, after=0)
-		tempHash = {"event_type" => "event_type", "data" => {"update" => {"fieldname"=>"#{fieldname}","before" => "#{before}",
-	          "after" =>  "#{after}","section"=>{},},},}
-	    if obj.instance_of?(Course)
-	    	tempHash["data"]["id"] = "#{obj.id}"
-	    	tempHash["data"]["name"] = "#{obj.name}"
-	    	tempHash["data"]["number"] = "#{obj.number}"
-	    elsif obj.instance_of?(Section)
-	    	tempHash["data"]["id"] = "#{obj.course.id}"
-	    	tempHash["data"]["name"] = "#{obj.course.name}"
-	    	tempHash["data"]["number"] = "#{obj.course.number}" 
-	    	tempHash["data"]["update"]["section"]["name"] = "#{obj.name}"
-	    	tempHash["data"]["update"]["section"]["crn"] = "#{obj.crn}"
-	    end
-	    Redis.current.publish("channel",tempHash.to_json)
-	end
 
+  def self.send_course_event(obj, field)
+    tempHash = {"id"=> obj.id, "name"=>obj.name, "number"=>obj.number,"update" => {"fieldname"=>field,},}
+    Redis.current.publish("channel",tempHash.to_json)
+  end
+
+  def self.send_section_event(obj, fieldtype)
+    tempHash = {"id" => obj.course.id, "name" => obj.course.name, "number" => obj.course.number,"update" => {
+      "seats"=> obj.seats, "seats_taken"=>obj.seats_taken,"section"=>
+        {"name" => obj.name, "crn" => obj.crn},},}
+    if fieldtype == :section_added or fieldtype == :section_removed
+      tempHash["update"]["field_name"] = fieldtype
+      Redis.current.publish("channel",tempHash.to_json)
+    else
+      if obj.seats_changed?
+        tempHash["before"] = :seats_was
+        if obj.seats_was > obj.seats
+          tempHash["update"]["field_name"] = :seats_removed
+        else 
+          tempHash["update"]["field_name"]= :seats_added
+        end
+        Redis.current.publish("channel",tempHash.to_json)
+        if obj.seats_taken >= obj.seats_was and obj.seats_taken < obj.seats
+          tempHash["update"]["field_name"] = :section_opened
+        elsif obj.seats_taken < obj.seats_was and obj.seats_taken >= obj.seats
+          tempHash["update"]["field_name"] = :section_closed
+        end
+        Redis.current.publish("channel",tempHash.to_json)
+      elsif obj.seats_taken_changed?
+        tempHash["before"] = :seats_taken_was
+        if obj.seats_taken_was >= obj.seats and obj.seats_taken < obj.seats
+          tempHash["update"]["field_name"] = :section_opened
+        elsif obj.seats_taken_was < obj.seats and obj.seats_taken >= obj.seats
+          tempHash["update"]["field_name"] = :section_closed
+        end
+        Redis.current.publish("channel",tempHash.to_json)
+      end
+    end
+  end 
 end
