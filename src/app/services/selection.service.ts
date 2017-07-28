07724c8838733f45d037e8e4f589ca93ca30e89b
Darren Lin <lin.darren95@gmail.com>
2017-07-28 15:41:29 -0400

    Made chhanges requested to PR #14

--- src/app/services/selection.service.ts
+++ src/app/services/selection.service.ts
@@ -13,20 +13,22 @@ export class SelectionService {
   public addSection(section : Section) {
     let store = JSON.parse(localStorage.getItem('selections')) || {};
     store[section.course_id] = store[section.course_id] || [];
-    if (store[section.course_id].includes(section.id)) return;
+    if (store[section.course_id].includes(section.id)) return false;
     store[section.course_id].push(section.id);
     store[section.course_id].sort();
     localStorage.setItem('selections', JSON.stringify(store));
+    return true;
   }
 
   public removeSection(section : Section) {
-    let store = JSON.parse(localStorage.getItem('selections'));
-    if (!store || !store[section.course_id] || !store[section.course_id].includes(section.id)) return;
+    let store = JSON.parse(localStorage.getItem('selections')) || {};
+    if (!store[section.course_id] || !store[section.course_id].includes(section.id)) return false;
     store[section.course_id].splice(store[section.course_id].indexOf(section.id), 1);
     if (store[section.course_id].length == 0) {
       delete store[section.course_id];
     }
     localStorage.setItem('selections', JSON.stringify(store));
+    return true;
   }
 
   public toggleCourse(course : Course) {
