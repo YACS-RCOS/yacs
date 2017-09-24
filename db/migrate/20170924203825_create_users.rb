7bf3d9dbd8dbb76f6c008a9c439d3b88e0faa28d
Yuze.Ma <584653629@qq.com>
2017-09-24 16:49:01 -0400

    Initialize the yacs-user system with inital db migration for user table

---
+++ db/migrate/20170924203825_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.timestamps
+    end
+    add_index :users, :username
+  end
+end
