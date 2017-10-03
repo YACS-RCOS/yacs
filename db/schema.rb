b8f1c7576aec3e3d11d8bf408e2be151b7ed401b
Yuze.Ma <584653629@qq.com>
2017-10-03 18:54:53 -0400

    Adding additional attribution: password for user table

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170924211516) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
+    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username"
