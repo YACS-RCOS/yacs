85b21ff97862cbff6489bfba8f40312d58682bc2
Yuze.Ma <584653629@qq.com>
2017-10-13 15:50:44 -0400

    Add devise module

--- db/migrate/20170924203825_create_users.rb
+++ db/migrate/20170924203825_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username, null: false, index: true
-      t.string :password, null: false
+      t.string :password, null: true
       t.timestamps
     end
   end
