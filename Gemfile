86a79d4bc9369404c67a8493abd575dc18cd202c
Yuze.Ma <584653629@qq.com>
2017-10-27 10:51:52 -0400

    Modified Gem file for the porject, yacs-auth is successfully added

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 3.0'
+gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -35,6 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise', '~> 4.3'
 gem 'yacs-auth', git:"git@github.com:YACS-RCOS/yacs-auth.git"
+gem 'jwt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,30 +1,49 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
   def new
+    # puts '------------------------START--------------------'
+    # self.resource = resource_class.new(sign_in_params)
+  # clean_up_passwords(resource)
+    # yield resource if block_given?
+    # respond_with(resource, serialize_options(resource))
+    # puts '------------------------DONE--------------------'
     super
   end
 
   # POST /resource/sign_in
   def create
-    puts 'Ayeeeeeeeeeeeee'
-    user = User.find_by(email: params[:email])
-    
-    if user.valid_password?(params[:password])
-      render json: user.as_json(), status: :created
-    else 
-      head(:unauthorized)
-    end
+    puts '------------------------START CREATE--------------------'
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+
+    # dummy = {sub: resource.id}
+    # puts JWT.encode(dummy, Yacs::Auth.config.secret, 'HS256')
+    puts Yacs::Auth.sign_in(resource)
+    # user = User.find_by(email: params[:user][:email])
+    # render json: user.as_json(), status: :created
+    puts '------------------------DONE CREATE--------------------'
+    # super
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    puts 'User Logged out'
+    super
+  end
 
-  # protected
 
+
+
+  protected
+  def after_sign_in_path_for(resource)
+    puts '------------------------AFTER SIGN IN--------------------'
+    stored_location_for(resource) || signed_in_root_path(resource)
+    # request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
