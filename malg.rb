4a48dbf2a47f2096816bee18a36f8a0c9b8c4071
Richie <richiejoeyoung@gmail.com>
2017-11-08 23:21:23 -0500

    adds add_record method with attribute source tracking

--- malg.rb
+++ malg.rb
@@ -1,10 +1,32 @@
 class Malg
-  def initialize
 
+  # class Graph
+  #   attr_accessor :graph, :sources
+  #   def initialize
+  #     @graph = { 'schools' => [], 'departments' => [], 'courses' => [], 'sections' => [] }
+  #     @sources {}
+  #   end
+
+  #   def add_record type, record, source
+  #     @graph[]
+  #   end
+  # end
+
+  def initialize
+    @graph = { 'schools' => [], 'departments' => [], 'courses' => [], 'sections' => [] }
+    @sources = {}
+  end
+
+  def add_record record, type, source, parent = nil
+    throw 'Nil Parent Error' if parent == nil && type != 'schools'
+    new_record = record.clone
+    @graph[type] << new_record
+    parent[type] << new_record if parent
+    @souces[new_record.object_id] = new_record.transform_values { |v| source }
   end
 
   def build_graph
-    graph = { 'schools' => [], 'departments' => [], 'courses' => [], 'sections' => [] }
+    
     default_order = sources.sort { |a, b| priorities.default(a) <=> priorities.default(b) }
 
     processed_sources = []
@@ -13,7 +35,7 @@ class Malg
       processed_sources << source.name
 
       source.data['schools'].each do |ss|
-        school = graph['schools'].detect { |gs| gs['name'] == ss['name'] }
+        school = @graph['schools'].detect { |gs| gs['name'] == ss['name'] }
 
         if school
           school['departments'] ||= []
@@ -34,28 +56,33 @@ class Malg
                     section = course['sections'].detect { |gs| gs['name'] == ss['name'] }
 
                     if section
-                      
+                      sc.each do |k, v|
+                        
+                      end
                     else
                       course['sections'] << ss
-                      graph['sections'] << ss
+                      @graph['sections'] << ss
                     end
                   end
                 else
                   department['courses'] << sc
-                  graph['courses'] << sc
+                  @graph['courses'] << sc
                 end
               end
             else
               school['departments'] << sd
-              graph['departments'] << sd
+              @graph['departments'] << sd
             end
           end
         else
-          graph['schools'] << ss
+          @graph['schools'] << ss
         end
       end
     end
   end
+
+
+
 
 
 
