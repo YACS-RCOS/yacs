0c6c40b051a5115fff56f230f03ece5df7db780b
Richie <richiejoeyoung@gmail.com>
2017-11-12 02:07:14 -0500

    adapt code from production script to pull from topics csv

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'oj'
+require './topics_csv_client'
 
 set :bind, '0.0.0.0'
 set :port, 4201
 
+ENV['CSV_SOURCE'] ||= 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRQUq7f_TXYzCy4nRh0GnIRgK31jbm_TIfjeaeJ_4-Ek26AUQ0S7Gkrw9DvwJGuu1-nXZDyk1FbZZWD/pub?gid=0&single=true&output=csv'
+topics_csv_client = TopicsCsvClient.new ENV['CSV_SOURCE']
+
 get "/" do 
-  json courses: []
+  json departments: topics_csv_client.courses_by_department
 end
---
+++ topics_csv_client.rb
@@ -0,0 +1,35 @@
+require 'csv'
+require 'open-uri'
+
+class TopicsCsvClient
+
+  def initialize url
+    @url = url
+  end
+
+  def courses
+    content = open(@url).read
+    csv = CSV.parse(content)
+    courses = csv[1..-1].map{|row| Hash[csv[0].zip(row)]}
+    courses.map! do |course|
+      if course['prerequisites']
+        course['description'] = "#{course['description']} Prerequisites: #{course['prerequisites']}"
+      end
+      course['min_credits'] = course['max_credits'] = course['credits']
+      course.compact!
+      course_numbers = course['number'].split('/')
+      course_numbers.map{|number| course.merge({'number' => number})}
+    end
+    courses.flatten!
+  end
+
+  def courses_by_department
+    departments = {}
+    courses.each do |course|
+      departments[course['department']] ||= []
+      departments[course['department']] << course
+      course.delete 'department'
+    end
+    departments.map { |k, v| { code: k, courses: v } }
+  end
+end
