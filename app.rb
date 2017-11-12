17228ccaf1fa33e28ed52bbdc1130f40b5ee8026
Richie <richiejoeyoung@gmail.com>
2017-11-12 01:43:10 -0500

    add source yaml file and serve its contents

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'oj'
+require 'yaml'
 
 set :bind, '0.0.0.0'
 set :port, 4201
 
+ENV['YAML_SOURCE'] ||= 'schools-and-departments.yml'
+
 get "/" do 
-  json courses: []
+  json YAML.load(open(ENV['YAML_SOURCE']))
 end
