951ec1e876e6577874ad7ea1e2547318962dde14
Richie <richiejoeyoung@gmail.com>
2017-12-07 03:34:09 -0500

    change port to 4600

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'oj'
 require './acalog_client'
 
 set :bind, '0.0.0.0'
-set :port, 4201
+set :port, 4600
 
 ENV['ACALOG_API_URI'] ||= "http://rpi.apis.acalog.com"
 ENV['ACALOG_API_KEY'] ||= "3eef8a28f26fb2bcc514e6f1938929a1f9317628"
