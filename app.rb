93ba261723939d02f66a912279db8e7c8b6044e8
Richie <richiejoeyoung@gmail.com>
2017-12-07 03:35:35 -0500

    change port to 4600

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'oj'
 require './topics_csv_client'
 
 set :bind, '0.0.0.0'
-set :port, 4201
+set :port, 4600
 
 ENV['CSV_SOURCE'] ||= 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRQUq7f_TXYzCy4nRh0GnIRgK31jbm_TIfjeaeJ_4-Ek26AUQ0S7Gkrw9DvwJGuu1-nXZDyk1FbZZWD/pub?gid=0&single=true&output=csv'
 topics_csv_client = TopicsCsvClient.new ENV['CSV_SOURCE']
