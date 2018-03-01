e0eca11a7400699bd19a3874b43275d79a5e17f9
Daniel Ackermans <ackerd2@rpi.edu>
2018-03-01 12:15:36 -0500

    Updated the locations database migration

--- db/migrate/20180228222337_add_periods_location_to_sections.rb
+++ db/migrate/20180228222337_add_periods_location_to_sections.rb
@@ -1,5 +1,5 @@
 class AddPeriodsLocationToSections < ActiveRecord::Migration[5.1]
   def change
-    add_column :sections, :periods_location, :string, array: true, default: []
+    add_column :sections, :periods_location, :string, array: true, default: [], null: false
   end
 end
