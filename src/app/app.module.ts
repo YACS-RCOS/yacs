392aca3ca737f0546bffcdd52e9773cdc55a0d5d
James Milne <milnej@rpi.edu>
2018-03-02 22:33:26 -0500

    Updates to app.module to begin implementation of notice bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRouterModule } from './app-router/module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/component';
+import { NoticeBarComponent } from './notice-bar/component';
 import { FooterComponent } from './footer/component';
 
 import { SchoolViewModule } from './school-view/module';
@@ -15,6 +16,7 @@ import { ScheduleViewModule } from './schedule-view/module';
 import { ConstantsService } from './services/constants';
 import { SelectionService } from './services/selection.service';
 import { ConflictsService } from './services/conflicts.service';
+import { NoticeService } from './services/notice.service';
 
 import { AboutComponent } from './about/component';
 
@@ -31,13 +33,15 @@ import { AboutComponent } from './about/component';
   declarations: [
     AppComponent,
     HeaderComponent,
+    NoticeBarComponent,
     FooterComponent,
     AboutComponent
   ],
   providers: [
     ConstantsService,
     SelectionService,
-    ConflictsService
+    ConflictsService,
+    NoticeService
   ],
   bootstrap: [AppComponent]
 })
