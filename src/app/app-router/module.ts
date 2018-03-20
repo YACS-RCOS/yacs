dee4046acbc6048e7165bec59863e04c9e805905
James Milne <milnej@rpi.edu>
2018-03-20 15:50:29 -0400

    Notice Bar Changes

--- src/app/app-router/module.ts
+++ src/app/app-router/module.ts
@@ -4,7 +4,6 @@ import { SchoolViewComponent } from '../school-view/component';
 import { CourseViewComponent } from '../course-view/component';
 import { ScheduleViewComponent } from '../schedule-view/component';
 import { AboutComponent } from '../about/component';
-import { NoticeBarComponent } from '../notice-bar/component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/schools', pathMatch: 'full' },
@@ -12,8 +11,6 @@ const routes: Routes = [
   { path: 'courses', component: CourseViewComponent },
   { path: 'schedules', component: ScheduleViewComponent },
   { path: 'about', component: AboutComponent},
-  //in for testing
-  { path: 'noticebar', component: NoticeBarComponent}
 ];
 
 @NgModule({
