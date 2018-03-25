852d77a65978973b0a4cbfc5ce8caa816410eec2
milnej <milnej@rpi.edu>
2018-03-25 16:12:16 -0400

    Begun implementation of typeahead search bar

--- src/app/header/component.ts
+++ src/app/header/component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { YacsService } from '../services/yacs.service';
+import { Course } from '../models/course.model';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'header-bar',
   templateUrl: './component.html',
+  providers: [YacsService],
   styleUrls: ['component.scss']
 })
 
 export class HeaderComponent {
 
-  constructor(private router: Router) { }
+  courses: Course[] = [];
+
+  constructor(
+    private router: Router, 
+    private config: NgbTypeaheadConfig,
+    private yacsService: YacsService) 
+  { 
+    //autocompletion of the first result
+    config.showHint = true;
+  }
 
   search(term: string) {
     this.router.navigate(['/courses'],
@@ -17,4 +34,15 @@ export class HeaderComponent {
         search: term
       }});
   }
-}
+
+  searchAhead = (text: Observable<string>) =>
+    text
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .switchMap(term =>
+        this.yacsService
+          .get('courses', text)
+          .then((data) => {
+            this.courses = data['courses'] as Course[];
+          }))
+  }
