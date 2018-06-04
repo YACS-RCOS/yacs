b6ab73674734a87a75c4a4a51d6f6a46733fcae6
bdieu178 <bdieu17@gmail.com>
2018-06-04 15:39:14 -0400

    Implemented Responder for Core

--- app/responders/core_responder.rb
+++ app/responders/core_responder.rb
@@ -1,33 +1,3 @@
-# frozen_string_literal
-
-# GoaL:Send a new Kafka message to topic(s), whenever an ActiveRecord object is changes state.
-#
-# Steps:(high level logic)
-#   1. Wait for a message from yacs-malg for change in state of an object.(Active Callback will be needed. What is the format of the data received?)
-#   2. If object changes state, then create a message in the format of [:uuid, :record]
-#   3. Using Kafka Responder methods to send data to topic
-#       Conditions:
-#           + Wha
-#
-#  TO DO:
-#   - implement file below
-#   - find out whether I need to create the topic as well
-#   - add tests to logic
-#
-#   Note: ensure each object type has it's own topic
-#         look into Active::Record::Serialization
-#
-#   Questions to answer:
-#      1. How will I be able to receive updates from the ActiveRecords ?
-#          - By using the
-#      2. What format is the current data input for the responder?
-#
-#      3. Where in this application is the whole entire record of data?
-#
-#      4. What conditions do we need to consider?
-
-#require 'json'
-
 class CoreResponder < ApplicationResponder
     #ALLOWED_TYPES = %w(school department course section).freeze
 
