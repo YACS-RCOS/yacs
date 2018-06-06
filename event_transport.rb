7ed74f76f68be5d0ee4b21507ae38269dae832f5
Richie <richiejoeyoung@gmail.com>
2018-06-05 21:34:06 -0400

    add a little error handling :winking_emoji:

--- event_transport.rb
+++ event_transport.rb
@@ -5,9 +5,15 @@ require 'oj'
 class EventTransport
   class << self
     %i(create update delete).each do |method|
-       define_method "send_#{method}" do |record, type|
-         type = ActiveSupport::Inflector.singularize type
-         WaterDrop::SyncProducer.call(Oj.dump({ 'type' => type, type => record, 'method' => method.to_s }), topic: 'full_transport') 
+      define_method "send_#{method}" do |record, type|
+        type = ActiveSupport::Inflector.singularize type
+        begin
+          WaterDrop::SyncProducer.call(Oj.dump({ 'type' => type, type => record, 'method' => method.to_s }), topic: 'full_transport')
+        rescue Exception => e
+          STDERR.puts "ERROR: Failed to deliver message. Will wait and try again.\nERROR: #{e}"
+          sleep 5
+          retry
+        end
       end
     end
   end
