4e655ee93ca15ee055c1b4ac1569d86afdbe8ded
bdieu178 <bdieu17@gmail.com>
2018-06-05 22:18:46 -0400

    Completed pull request changes

--- app/responders/core_responder.rb
+++ app/responders/core_responder.rb
@@ -15,5 +15,4 @@ module EventResponders
            respond_to :section_change , event.to_json(:only => [:uuid,:course_id, :name, :crn, :seats, :seats, :seats_taken, :created_at, :updated_at, :num_periods, :instructors, :conflicts ])
        end
     end
-
 end
