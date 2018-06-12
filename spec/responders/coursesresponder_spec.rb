2be05166dc33d588ecd6034ffc3209a98ee6e287
bdieu178 <bdieu17@gmail.com>
2018-06-11 20:59:14 -0400

    Added environment variables for kafka topics within the responders

--- spec/responders/coursesresponder_spec.rb
+++ spec/responders/coursesresponder_spec.rb
@@ -4,20 +4,21 @@ RSpec.describe CoursesResponder do
     subject(:responder) {described_class.new}
     #describe the  instance of Course.self
     let(:event) { Course.self }
+    let(:topic_var) { ENV["COURSES_TOPIC_NAME"]}
 
     context 'produce message for topic' do
 
     it { expect(described_class.topcis.size).to eq 1 }
 
         describe 'topic(s) to speak to' do
-            let(:topic) { described_class.topics['course_change'] }
-            it { expect(topic.name).to eq 'course_change' }
+            let(:topic) { described_class.topics[topic_var] }
+            it { expect(topic.name).to eq topic_var }
         end
 
         describe '#call' do
             let(:input_data) { { rand => rand } }
             let(:accumulated_data) do
-                [[event.to_json, { topic: 'section_change'}]]
+                [[event.to_json, { topic: topic_var }]]
             end
 
             it 'expect to add builds to message buffer' do
