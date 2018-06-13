8f0e88f7fabfa555eda5b31f99a2f0c4d2547c87
Bryan Dieudonne <bdieu17@gmail.com>
2018-06-12 21:07:35 -0400

    Update sections_responder.rb

--- app/responders/sections_responder.rb
+++ app/responders/sections_responder.rb
@@ -4,7 +4,7 @@ class SectionsResponder < ApplicationResponder
   topic @@topic_name, required: true
 
   def respond(event)
-    respond_to :"#{topic_name}" , event.to_json(:only => [:uuid,:course_id, :name, :crn, :seats, :seats, :seats_taken, :created_at, :updated_at, :num_periods, :instructors, :conflicts])
+    respond_to @@topic_name, event.to_json(:only => [:uuid,:course_id, :name, :crn, :seats, :seats, :seats_taken, :created_at, :updated_at, :num_periods, :instructors, :conflicts])
   end
 end
 
