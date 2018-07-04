8594cb9362fdba4a443b77e5c0932c7df7870a02
bdieu178 <bdieu17@gmail.com>
2018-07-04 18:52:59 -0400

    Added initial files

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby '2.5.1'
 gem 'karafka'
 
+# Plezi Websocket
+gem 'plezi'
---
+++ app/websocket/websocket.rb
@@ -0,0 +1,22 @@
+## Client side
+## Javascript file to emit an event in JSON format 
+
+#client.emit(
+#    {
+#        event: "notification" , 
+#        data: "the consumer response" 
+#    }
+# )
+
+
+##JSON Echo Server
+
+class EventMessages
+    #auto_dispatch
+    # ...
+    def #...
+    end
+
+end 
+
+Plexi.route '/' , EventMessages 
