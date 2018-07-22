fee69cc85521d626776d7c60c8b31011d341b55b
Bryan Dieudonne <bdieu17@gmail.com>
2018-07-22 17:24:13 -0400

    Update sections_consumer.rb

--- app/consumers/sections_consumer.rb
+++ app/consumers/sections_consumer.rb
@@ -4,11 +4,11 @@ require 'eventstream'
 
 class SectionConsumer < ApplicationConsumer
   def consume
-		if ( Object.const_defined?('EventStream') == false )
-			puts "EventStream class not initialized" 
-		else 
-			EventStream.on_message(params)
-			puts "Consumer message sent to websocket"
-	  end	
+    if ( Object.const_defined?('EventStream') == false )
+      puts "EventStream class not initialized" 
+    else 
+      EventStream.on_message(params)
+      puts "Consumer message sent to websocket"
+    end	
   end
 end
