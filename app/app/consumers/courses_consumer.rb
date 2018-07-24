4c43dc7916bbc1a0e7f5044273787dd6d3c37c18
bdieu178 <bdieu17@gmail.com>
2018-07-24 19:47:09 -0400

    Fixed styling

--- app/app/consumers/courses_consumer.rb
+++ app/app/consumers/courses_consumer.rb
@@ -5,10 +5,10 @@ require 'plezi'
 class CourseConsumer < ApplicationConsumer
   def consume
 	  if ( Object.const_defined?('EventStream') == false )
-	  puts "EventStream class not initialized" 
-	else 
-	  EventStream.on_message(params)
-	  puts "Consumer message sent to websocket"
-	end	
+	    puts "EventStream class not initialized" 
+	  else 
+	    EventStream.on_message(params)
+	    puts "Consumer message sent to websocket"
+	  end	
   end
 end
