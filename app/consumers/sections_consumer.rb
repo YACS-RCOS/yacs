a4b9490799165644fce8fdd35a8f82777e711758
bdieu178 <bdieu17@gmail.com>
2018-07-25 09:41:18 -0400

    Fixed consumers & eventsream classes

--- app/consumers/sections_consumer.rb
+++ app/consumers/sections_consumer.rb
@@ -1,14 +1,11 @@
 require 'karafka'
+require 'plezi'
+require 'iodine'
 require_relative 'application_consumer'
-require 'eventstream'
 
 class SectionConsumer < ApplicationConsumer
   def consume
-    if ( Object.const_defined?('EventStream') == false )
-      puts "EventStream class not initialized" 
-    else 
-      EventStream.on_message(params)
-      puts "Consumer message sent to websocket"
-    end	
+    EventStream.new.notify(params)
+    puts "Consumer message sent to websocket"	
   end
 end
