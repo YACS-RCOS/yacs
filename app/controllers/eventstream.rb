47a278056a2d947c3e1a651cb1053c9c8a5dc5ee
bdieu178 <bdieu17@gmail.com>
2018-07-24 21:42:14 -0400

    Removed app/app folder

--- app/controllers/eventstream.rb
+++ app/controllers/eventstream.rb
@@ -1,16 +1,22 @@
 require 'plezi'
+require 'iodine'
+require 'json'
 
 class EventStream
-
+  @auto_dispatch = true
+  
   def on_open
     puts 'Opened the YACS-EventStream Websocket!'
   end
 
-  ## When receiving a single message from Consumer pusblish to socket
-  def on_message data
-    unless write { notifications: :data }.to_json
-      write "no message sent"
-    end
+  def notify(data)
+    @event = data
+    puts data
+  end 
+  
+  #send data to websocket
+  def send_data
+    write @event
   end 
 
   def on_close
