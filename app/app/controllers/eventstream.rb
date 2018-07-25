86fcd3150532ae84273d9044870ea538a1f5fdd6
bdieu178 <bdieu17@gmail.com>
2018-07-24 21:32:02 -0400

    Resolved issue #7 & #8, but need to format JSON output

--- app/app/controllers/eventstream.rb
+++ app/app/controllers/eventstream.rb
@@ -3,21 +3,22 @@ require 'iodine'
 require 'json'
 
 class EventStream
-  #@auto_dispatch = true
+  @auto_dispatch = true
   
   def on_open
     puts 'Opened the YACS-EventStream Websocket!'
   end
 
-  #def on_message data
   def notify(data)
+    @event = data
     puts data
-    #event = {:notifications => data }
-    #puts event.to_json  # need to figure out this shit
+  end 
+  
+  #send data to websocket
+  def send_data
+    write @event
   end 
 
   def on_close
   end
 end
-#Plezi.route '/notifications', EventStream
-#ip address 172.19.0.9:9094
