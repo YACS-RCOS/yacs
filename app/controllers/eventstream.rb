2b9330ef266c8902e8c68b1c8c96a86931797b02
bdieu178 <bdieu17@gmail.com>
2018-08-07 10:22:23 -0400

    Updated Kafka consumers to push directly to Iodine pub/sub

--- app/controllers/eventstream.rb
+++ app/controllers/eventstream.rb
@@ -2,29 +2,27 @@ require 'plezi'
 require 'iodine'
 
 class EventStream
-  @auto_dispatch = true
+  #@auto_dispatch = true
   def index
     render 'client'
   end 
 
-  #def on_open
-  #  puts "WS connection open"
-  #  ::Iodine::subscribe channel: "notifications"
-  #end
-
-  def notify(data)
-    @notification = data.to_s
-    ::Iodine::publish channel: "notifications", message: "#{@notification}"
+  def on_open
+    puts "WS connection open"
+    ::Iodine::subscribe channel:"notifications" do
+      puts "I'm in!"
+    end
   end
   
-  #def on_message
-  #  ::Iodine::subscribe :notifications
-  #  ::Iodine::publish :notifications , "#{@data}"
-  #end 
+  #TODO
+  def on_message data
+    #::Iodine::write "#{@data}"
+    #puts data
+  end 
 
-  #def on_close 
-  #  publish "notifications"
-  #end 
+  def on_close
+    ::Iodine::unsubscribe("notifications")
+  end 
 end
 #test location: ws://localhost:3000/notifications/
 
