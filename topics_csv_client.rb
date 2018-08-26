29a3e68408e21abff8742a1fff3e1ffb3043e125
ada young <ada@adadoes.io>
2018-08-26 15:51:26 -0400

    multi semester support, ruby 2.5.1

--- topics_csv_client.rb
+++ topics_csv_client.rb
@@ -3,19 +3,22 @@ require 'open-uri'
 
 class TopicsCsvClient
 
-  def initialize url
-    @url = url
+  def initialize term_shortname
+    @url = ENV["CSV_SOURCE_#{term_shortname}"]
   end
 
   def courses
+    return [] unless @url
     content = open(@url).read
+    puts content
     csv = CSV.parse(content)
-    courses = csv[1..-1].map{|row| Hash[csv[0].zip(row)]}.reject{|c| !c['department'] || !c['number']}
+    courses = csv[1..-1].map{|row| Hash[csv[0].zip(row)]}.reject{|c| !c['subject'] || !c['number']}
     courses.map! do |course|
       if course['prerequisites']
         course['description'] = "#{course['description']} Prerequisites: #{course['prerequisites']}"
       end
       course['min_credits'] = course['max_credits'] = course['credits']
+      course['tags'] = ['topics']
       course.compact!
       course_numbers = course['number'].split('/')
       course_numbers.map{|number| course.merge({'number' => number})}
@@ -23,13 +26,13 @@ class TopicsCsvClient
     courses.flatten!
   end
 
-  def courses_by_department
-    departments = {}
+  def courses_by_subject
+    subjects = {}
     courses.each do |course|
-      departments[course['department']] ||= []
-      departments[course['department']] << course
-      course.delete 'department'
+      subjects[course['subject']] ||= []
+      subjects[course['subject']] << course
+      course.delete 'subject'
     end
-    departments.map { |k, v| { code: k, courses: v } }
+    subjects.map { |k, v| { code: k, courses: v } }
   end
 end
