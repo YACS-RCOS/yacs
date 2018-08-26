caa87063d049bca6abc5b96d35732f8e559add44
ada young <ada@adadoes.io>
2018-08-26 15:51:26 -0400

    multi semester support, ruby 2.5.1

--- app.rb
+++ app.rb
@@ -6,9 +6,6 @@ require './topics_csv_client'
 set :bind, '0.0.0.0'
 set :port, 4600
 
-ENV['CSV_SOURCE'] ||= 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRQUq7f_TXYzCy4nRh0GnIRgK31jbm_TIfjeaeJ_4-Ek26AUQ0S7Gkrw9DvwJGuu1-nXZDyk1FbZZWD/pub?gid=0&single=true&output=csv'
-topics_csv_client = TopicsCsvClient.new ENV['CSV_SOURCE']
-
-get "/" do 
-  json departments: topics_csv_client.courses_by_department
+get "/:term_shortname" do
+  json subjects: TopicsCsvClient.new(params[:term_shortname]).courses_by_subject
 end
