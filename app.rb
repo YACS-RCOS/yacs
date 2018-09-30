31c3b38070bd189b5fd76ce4cb25bf917210b1a0
ada young <ada@adadoes.io>
2018-09-29 23:34:50 -0400

    update for api v6

--- app.rb
+++ app.rb
@@ -7,5 +7,5 @@ set :bind, '0.0.0.0'
 set :port, 4600
 
 get "/:term_shortname" do
-  json subjects: TopicsCsvClient.new(params[:term_shortname]).courses_by_subject
+  json subjects: TopicsCsvClient.new(params[:term_shortname]).listings_by_subject
 end
