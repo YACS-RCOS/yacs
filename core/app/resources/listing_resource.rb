52e7e415e6745ce611913bcdd06aa235166d8035
ada young <ada@adadoes.io>
2018-09-30 00:40:27 -0400

    upgrade to redis 5 and postgres 10.5. fix listing resource. TODO: open pr to graphiti for jsonb support

--- core/app/resources/listing_resource.rb
+++ core/app/resources/listing_resource.rb
@@ -1,13 +1,12 @@
 class ListingResource < ApplicationResource
-  attribute :shortname, :string
   attribute :longname, :string
   attribute :description, :string
   attribute :min_credits, :integer
   attribute :max_credits, :integer
   attribute :active, :boolean
   attribute :tags, :array
-  attribute :auto_attributes, :hash
-  attribute :override_attributes, :hash
+  # attribute :auto_attributes, :hash
+  # attribute :override_attributes, :hash
   attribute :uuid, :string
   attribute :course_id, :integer
   attribute :term_id, :integer
