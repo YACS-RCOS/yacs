18727df6ec3f69d231c522fba5a6b8a58dce7ea8
Max Schwartz <schwam4@rpi.edu>
2018-10-17 17:41:48 -0400

    Updated index.ts
    included courses, listings, subjects, terms

--- packages/yacs-api-client/index.ts
+++ packages/yacs-api-client/index.ts
@@ -1,1 +1,5 @@
 export { School } from "./lib/school";
+export { Course	} from "./lib/course";
+export { Subject } from "./lib/subject";
+export { Listing } from "./lib/listing";
+export { Term } from "./lib/term";
