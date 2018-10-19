6e10c5c664ea8554389aa4ab9f8efc3bc6f55c41
Albert Liu <albertymliu@gmail.com>
2018-10-18 23:22:17 -0400

    added some placeholder stuff

---
+++ adapters/nyu/gallatin/main.py
@@ -0,0 +1,9 @@
+import json
+
+URL = "https://gallatin.nyu.edu/academics/courses/jcr:content/content/search.json"
+
+def create_query(query):
+	# Placeholder function while we still don't really know
+	# How to query the db
+	global URL
+	return URL+'?'+query
