011a581f35c387563ab0f83231d108703fd4d682
Evan Silverman <es4753@nyu.edu>
2018-10-23 14:56:27 -0400

    Remove all instances of URL
    She doesn't even go here

--- pipelines/nyu/adapters/gallatin/apitrans.py
+++ pipelines/nyu/adapters/gallatin/apitrans.py
@@ -1,9 +1,6 @@
-from globals import API_URL
-
 def get_query(term_shortname): #In the format YYYYTT
-	global API_URL
 	year = term_shortname[:-2]
 	term = term_shortname[4:]  #SP = spring, FA = fall, SU = summer, WI = winter
 
   # As a shortcut, we can use limit=0 to get all of the courses
-	return API_URL + "?limit=0&year=" + year + "&term=" + term
+	return "?limit=0&year=" + year + "&term=" + term
