964fcb066015c8c41dbe495f66c745c325033e8a
Albert Liu <albertymliu@gmail.com>
2018-10-23 18:03:22 -0400

    added some simple stuff as skeleton of main

--- pipelines/nyu/adapters/gallatin/main.py
+++ pipelines/nyu/adapters/gallatin/main.py
@@ -1,2 +1,17 @@
 from http.server import HTTPServer
 from apitrans import get_query
+from reqhandler import ReqHandler
+
+
+def run():
+  print('starting server...')
+
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, ReqHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+
+run()
