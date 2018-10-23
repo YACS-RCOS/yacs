ce58e7f096333da16155e546f3acc40b708ad02b
Evan Silverman <es4753@nyu.edu>
2018-10-23 15:02:39 -0400

    Update apitrans.py

--- pipelines/nyu/adapters/gallatin/apitrans.py
+++ pipelines/nyu/adapters/gallatin/apitrans.py
@@ -1,3 +1,6 @@
+# This function takes in the term_shortname as a string
+# and then returns a query string to send to the API
+
 def get_query(term_shortname): #In the format YYYYTT
 	year = term_shortname[:-2]
 	term = term_shortname[4:]  #SP = spring, FA = fall, SU = summer, WI = winter
