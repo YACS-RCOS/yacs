a31aab9b2eb9e20c7557e83d779f2e6d63cb714f
Albert Liu <albertymliu@gmail.com>
2018-10-23 00:48:20 -0400

    changed for readability

--- pipelines/nyu/adapters/gallatin/apitrans.py
+++ pipelines/nyu/adapters/gallatin/apitrans.py
@@ -1,6 +1,9 @@
+from globals import API_URL
+
 def get_query(term_shortname): #In the format YYYYTT
-    year = term_shortname[:-2]
-    term = term_shortname[4:]  #SP = spring, FA = fall, SU = summer, WI = winter
+	global API_URL
+	year = term_shortname[:-2]
+	term = term_shortname[4:]  #SP = spring, FA = fall, SU = summer, WI = winter
 
     # As a shortcut, we can use limit=0 to get all of the courses
-    return "https://gallatin.nyu.edu/academics/courses/jcr:content/content/search.json?limit=0&year=" + year + "&term=" + term
+	return API_URL+ "?limit=0&year=" + year + "&term=" + term
