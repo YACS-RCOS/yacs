cfec4da4d3b8582731ddb8ea4c7941b359c501be
Albert Liu <albertymliu@gmail.com>
2018-10-23 17:06:57 -0400

    added beginnings of reqhandler

---
+++ pipelines/nyu/adapters/gallatin/reqhandler.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+# So we need to override the do_GET() method in BaseHTTPRequestHandler
+# We need to then fetch the appropriate data
+# Then finally we need to send it back
+
+# Here's some resources I've found so far
+# https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+# https://gist.github.com/bradmontgomery/2219997
+# 
