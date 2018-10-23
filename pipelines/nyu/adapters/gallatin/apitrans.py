63adafd2fc1b4a4b05fb1f3f1f6c673dd44aa70c
Evan Silverman <es4753@nyu.edu>
2018-10-23 15:08:25 -0400

    Update apitrans.py

--- pipelines/nyu/adapters/gallatin/apitrans.py
+++ pipelines/nyu/adapters/gallatin/apitrans.py
@@ -2,8 +2,8 @@
 # and then returns a query string to send to the API
 
 def get_query(term_shortname): #In the format YYYYTT
-	year = term_shortname[:-2]
-	term = term_shortname[4:]  #SP = spring, FA = fall, SU = summer, WI = winter
+  year = term_shortname[:-2]
+  term = term_shortname[4:]. #SP = spring, FA = fall, SU = summer, WI = winter
 
   # As a shortcut, we can use limit=0 to get all of the courses
-	return "?limit=0&year=" + year + "&term=" + term
+  return "?limit=0&year=" + year + "&term=" + term
