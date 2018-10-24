12d5ad179a7ba412c13302a03763e29a44092dfb
Albert Liu <albertymliu@gmail.com>
2018-10-23 20:35:50 -0400

    added test function to make testing json translation easier

--- pipelines/nyu/adapters/gallatin/test.py
+++ pipelines/nyu/adapters/gallatin/test.py
@@ -7,5 +7,12 @@ PATH = ''
 # Test script to make sure that everything is running according to plan
 
 # Send a request to the server and print it
-r = requests.get("{}:{}/{}".format(SERVER_URL,PORT,PATH))
-print(r.text)
+def try_connect():
+	r = requests.get("{}:{}/{}".format(SERVER_URL,PORT,PATH))
+	print(r.text)
+
+# Get test data as if you had connected to the Gallatin API
+def get_test_data():
+	with open("data/data.json","r") as f:
+		data = f.read()
+	return data
