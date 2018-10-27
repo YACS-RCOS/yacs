fd0875782fec52cafc25c0bfac54ac6b66631fdb
Albert Liu <albertymliu@gmail.com>
2018-10-26 21:44:33 -0400

    added division of labor for standard function

--- pipelines/nyu/adapters/gallatin/jsontrans.py
+++ pipelines/nyu/adapters/gallatin/jsontrans.py
@@ -1,4 +1,20 @@
+import pandas as pd
+import json
+
 def format_data(unformatted_json):
 	# This function takes in a JSON string in the API's format
 	# and returns a JSON string in Yacs format
+
+	# TODO Figure out how to divide work
+	# between into pandas and json effectively
+	data = json.loads(unformatted_json)
+
+	df = pd.from_json(unformatted_json)
+
 	return "FORMATTED_JSON_STRING"
+
+def rename_attributes(unformatted_json):
+	return "correctly named attributes"
+
+def filter_results(unfiltered_json):
+	return "filtered JSON"
