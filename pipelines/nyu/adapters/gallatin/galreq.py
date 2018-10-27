25ead4ec88c84129cf6592bc629dd4e503b7eaed
Albert Liu <albertymliu@gmail.com>
2018-10-27 19:38:39 -0400

    formatting

--- pipelines/nyu/adapters/gallatin/galreq.py
+++ pipelines/nyu/adapters/gallatin/galreq.py
@@ -4,9 +4,9 @@ from  globals import API_URL
 def gallatin_data(query_string):
 	# This function takes in a query string
 	# and returns the JSON data that the API returns for that query string
-  
-  #getting json file from gallatin
-  r = requests.get(API_URL + query_string)
 
-  #returning json data
-  return r.text.strip()
+	#getting json file from gallatin
+	r = requests.get(API_URL + query_string)
+
+	#returning json data
+	return r.text.strip()
