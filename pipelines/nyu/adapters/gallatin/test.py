c5ce41782c9d046e2473985059d0f887e17e2f77
Albert Liu <albertymliu@gmail.com>
2018-10-28 12:49:37 -0400

    added display settings function and some debugging print statements

--- pipelines/nyu/adapters/gallatin/test.py
+++ pipelines/nyu/adapters/gallatin/test.py
@@ -2,20 +2,23 @@ import requests # Python Packages
 import os
 import sys
 import warnings
+import pandas as pd
 
 # Test script to make sure that everything is running according to plan
 
 # Getting/setting path vars
 try:
+	print('Setting up path variables...')
 	path = os.path.abspath(__file__) # Run the first time using python -i test.py
 	dirname = os.path.dirname(path)
 	src = os.path.join(dirname,'src')
 	sys.path.append(src)
 except:
-	pass # if __file__ is undefined, then the script is being run in interpreter -
+	print('Skipped path variable setup: variables already defined.')
+	# if __file__ is undefined, then the script is being run in interpreter -
 	# 'path' should already be defined then
 
-
+print('Setting up global environment...')
 # Setting convenience Global vars
 jst = os.path.join(src,'jsontrans.py') # shorthand for jsontrans module
 
@@ -24,6 +27,7 @@ SERVER_URL = '127.0.0.1'
 PORT = 8080
 INDEX = ''
 e = exec
+PD_DEFAULTS = (0, 60, 80)
 
 # Useful Utility Functions
 
@@ -37,11 +41,28 @@ def r(filename):
 		txt = f.read()
 	return txt
 
+def pd_settings(max_cols = 0,max_rows = None,disp_width = None):
+
+    # Set pandas display settings and retrieve old settings
+    initial_max_cols = pd.get_option('display.max_columns')
+    initial_max_rows = pd.get_option('display.max_rows')
+    initial_width = pd.get_option('display.width')
+
+    pd.set_option('display.max_columns', max_cols)
+    pd.set_option('display.max_rows', max_rows)
+    if disp_width is not None:
+        pd.set_option('display.width', disp_width)
+    return (initial_max_cols,initial_max_rows,initial_width)
+
+def pd_reset():
+	return pd_settings(*PD_DEFAULTS)
+
 # Getting Cython to chill out
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 # importing resources from local modules in file system
+print('Reloading packages...')
 from apitrans import get_query
 from galreq import gallatin_data
 from globals import *
@@ -51,4 +72,6 @@ import get_new_data
 from get_new_data import fetch_data,get_data
 get_new_data.dirname = dirname
 
+print('Setting display settings...')
+pd_settings(0,None,50)
 print('test.py was run successfully')
