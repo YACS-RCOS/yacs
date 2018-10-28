1261039bb459df59bc12856124331e0a73ec0c47
Albert Liu <albertymliu@gmail.com>
2018-10-28 10:55:22 -0400

    added more functionality

--- pipelines/nyu/adapters/gallatin/get_data.py
+++ pipelines/nyu/adapters/gallatin/get_data.py
@@ -3,15 +3,23 @@ from globals import API_URL
 import os
 
 QUERY = "?limit=1"
-filename = "data.json"
 
-filedir = os.path.dirname(os.path.abspath(__file__))
-datadir = os.path.join(filedir,"data")
-try:
-	os.mkdir(datadir)
-except:
-	pass
+def get_data(filename = 'data/data.json'):
+	with open(filename,"r") as f:
+		txt = f.read()
+	return txt
 
-r = requests.get(API_URL+QUERY)
-with open(os.path.join(datadir,filename),"w") as f:
-	f.write(r.text)
+def fetch_data(query, filename = "data.json"):
+	global path,datadir
+	try:
+		path = os.path.dirname(os.path.abspath(__file__))
+		datadir = os.path.join(path,"data")
+		os.mkdir(datadir)
+	except:
+		pass
+
+	r = requests.get(API_URL+query)
+	if filename is not None:
+		with open(os.path.join(datadir,filename),"w") as f:
+			f.write(r.text)
+	return r.text
