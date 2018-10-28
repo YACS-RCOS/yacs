cffd55eee042b34bc3ebcaba90402d35d747d514
Albert Liu <albertymliu@gmail.com>
2018-10-28 12:50:15 -0400

    updated for flexibility:

--- pipelines/nyu/adapters/gallatin/get_new_data.py
+++ pipelines/nyu/adapters/gallatin/get_new_data.py
@@ -2,17 +2,25 @@ import requests
 from globals import API_URL
 import os
 
-def get_data(filename = 'data/data.json'):
-	with open(filename,"r") as f:
+dirname = None
+datadir = None
+
+def get_datadir():
+	global dirname,datadir
+	if dirname is None:
+		dirname = os.path.dirname(os.path.abspath(__file__))
+	if datadir is None:
+		datadir = os.path.join(dirname,"data")
+	return datadir
+
+
+def get_data(filename = 'data.json'):
+	with open(os.path.join(get_datadir(),filename),"r") as f:
 		txt = f.read()
 	return txt
 
 def fetch_data(query, filename = "data.json"):
-	global dirname,datadir
-	if dirname is None:
-		path = os.path.abspath(__file__)
-		dirname = os.path.dirname(path)
-	datadir = os.path.join(dirname,"data")
+	datadir = get_datadir()
 	try:
 		os.mkdir(datadir)
 	except:
