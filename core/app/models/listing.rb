f6ae5865425321f41855534b255436daf3654750
ada young <ada@adadoes.io>
2018-10-28 11:49:27 -0400

    multi sem temporary workaround. karafka sucks.

--- core/app/models/listing.rb
+++ core/app/models/listing.rb
@@ -3,6 +3,7 @@ class Listing < ActiveRecord::Base
   belongs_to :term
   has_many   :sections, dependent: :destroy
   validates  :longname, presence: true
+  validates :course_id, uniqueness: { scope: :term_id}
 
   scope :latest, ->(value) {
     joins(:term).where('terms.shortname = (SELECT MAX(terms.shortname) FROM terms WHERE listings.term_id = terms.id AND listings.active = true)')
