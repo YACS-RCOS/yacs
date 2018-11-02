a88efafce800b1a3bfbeb931a01207588b2ac2b2
Albert Liu <albertymliu@gmail.com>
2018-11-02 19:16:46 -0400

    added all the skeletons

---
+++ pipelines/nyu/adapters/course_catalog/src/append_data.py
@@ -0,0 +1,7 @@
+# Task 3
+def append_data(subject_dict, entry_dict):
+	# subject_dict and entry_dict are both dictionaries
+	# Uses information stored in the entry_dict to
+	# update the subject_dict
+	# i.e. adding entries
+	return None
---
+++ pipelines/nyu/adapters/course_catalog/src/get_records.py
@@ -0,0 +1,4 @@
+def get_records(raw_html):
+	#input is raw HTML (string)
+	# Yields a dictionary with all the data for a record
+	yield "ENTRY_DICT"
