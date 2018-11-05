7a232d055fda3a9c4e0ba431a6ef22fa1aaa99f8
Albert Liu <albertymliu@gmail.com>
2018-11-04 22:32:26 -0500

    added comments

--- pipelines/nyu/adapters/course_catalog/src/append_data.py
+++ pipelines/nyu/adapters/course_catalog/src/append_data.py
@@ -4,4 +4,10 @@ def append_data(subject_dict, entry_dict):
 	# Uses information stored in the entry_dict to
 	# update the subject_dict
 	# i.e. adding entries
+
+	# 1 Check if section is in subject_dict
+
+	# add subject plus subject attributes
+	
+
 	return None
--- pipelines/nyu/adapters/course_catalog/src/get_records.py
+++ pipelines/nyu/adapters/course_catalog/src/get_records.py
@@ -2,3 +2,9 @@ def get_records(raw_html):
 	#input is raw HTML (string)
 	# Yields a dictionary with all the data for a record
 	yield "ENTRY_DICT"
+
+#
+#
+#
+#
+#
