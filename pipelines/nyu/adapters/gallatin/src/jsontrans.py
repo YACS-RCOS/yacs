c59746dfef3a18f7a87da2f8a6516ea757cd7a75
Albert Liu <albertymliu@gmail.com>
2018-11-04 22:32:00 -0500

    added filters, hitting brick wall bc gallatin

--- pipelines/nyu/adapters/gallatin/src/jsontrans.py
+++ pipelines/nyu/adapters/gallatin/src/jsontrans.py
@@ -1,127 +1,143 @@
 import pandas as pd
 import json
+import re
+import copy
 
-SUBJECTS = ['Interdisciplinary Seminars (IDSEM-UG)',
-       'Advanced Writing Courses (WRTNG-UG)', 'Arts Workshops (ARTS-UG)',
-       'Individualized Projects  (INDIV-UG)',
-       'Global Programs (INDIV-UG)',
-       'First-Year Program: Writing Seminars (FIRST-UG)',
-       'Global Programs (PRACT-UG)', 'Graduate Core (CORE-GG)',
-       'Leaves and Sabbaticals (LEAVE-XX)',
-       'Graduate Electives (ELEC-GG)', 'Global Programs (WRTNG-UG)',
-       'First-Year Program: Interdisciplinary Seminars (FIRST-UG)',
-       'Global Programs (SASEM-UG)', 'Travel Courses  (TRAVL-UG)',
-       'Individualized Projects  (INDIV-GG)',
-       'Global Programs (IDSEM-UG)', 'Practicum (PRACT-UG)',
-       'Colloquium (COLLQ-UG)',
-       'First-Year Program: Transfer Student Research Seminar (FIRST-UG)',
-       'Community Learning Courses (CLI-UG)']
-	   # TODO extract all the subject names and make them into a list for easier access
-SUBJECTS_LONG = ['Interdisciplinary Seminars', 'Advanced Writing Courses',
-				'Arts Workshops', 'Individualized Projects ', 'Global Programs',
-				'First-Year Program: Writing Seminars', 'Global Programs',
-				'Graduate Core', 'Leaves and Sabbaticals', 'Graduate Electives',
-				'Global Programs', 'First-Year Program: Interdisciplinary Seminars',
-				'Global Programs', 'Travel Courses ', 'Individualized Projects ',
-				'Global Programs', 'Practicum', 'Colloquium',
-				'First-Year Program: Transfer Student Research Seminar',
-				'Community Learning Courses']
-
-SUBJECTS_SHORT = ['IDSEM', 'WRTNG', 'ARTS', 'INDIV',
-				'INDIV', 'FIRST', 'PRACT', 'CORE',
-				'LEAVE', 'ELEC', 'WRTNG', 'FIRST',
-				'SASEM', 'TRAVL', 'INDIV', 'IDSEM',
-				'PRACT', 'COLLQ', 'FIRST', 'CLI']
-
-SUBJECT_IS_UG = [True, True, True, True, True, True,
-				True, False, False, False, True, True,
-				True, True, False, True, True, True, True, True]
+index = {}
+subject_list = [] # This is global only for testing purposes
 
 def format_data(unformatted_json):
 	# This function takes in a JSON string in the API's format
 	# and returns a JSON string in Yacs format
+	df = get_df(unformatted_json) # Get a pandas dataframe to do operations on
 
-	df = get_df(unformatted_json)
-
-	# For testing purposes only
-	df['description'] = df['description'].str[0]
-	return df
-	# TODO Rename all the columns
-
-	# TODO Use pivot to reformat the data rows
-
-	# Original
-	# Index(['course', 'credit', 'days', 'days2', 'description',
-	#    'foundation-libarts', 'instructors', 'level', 'notes', 'section',
-	#    'term', 'times', 'title', 'type', 'year'],
-	#   dtype='object')
-	# https://stackoverflow.com/questions/40470954/convert-pandas-dataframe-to-nested-json
-	# https://stackoverflow.com/questions/46205399/how-to-generate-n-level-hierarchical-json-from-pandas-dataframe
-	j = (df.groupby(['ID','Location','Country','Latitude','Longitude'], as_index=False)
-	     .apply(lambda x: x[['timestamp','tide']].to_dict('r'))
-		.reset_index()
-         .rename(columns={0:'Tide-Data'})
-         .to_json(orient='records'))
+	global subject_list
 
 
 
-	# Desired
-	# "subjects": [
-	# 	{
-	# 	"shortname": "ARTS-UG","longname": "Arts Workshops",
-	# 	"listings": [
-	# 			{
-	# 				"shortname": "1485","longname": "FullCourseName","min_credits": 4,"max_credits": 4,
-	# 				"description": "Course desc"
-	# 				"sections": [
-	# 					"shortname": "001",
-	# 					"periods": [
-	# 						{...}
-	# 					]
-	# 				]
-	# 			}
-	# 		]
-	# 	},
-	# 	{
-	# 		"shortname": "IDSEM-UG","longname": "Interdisciplinary Seminars",
-	# 		"listings": [
-	# 			...
-	# 		]
-	# 	}
-	# ]
 
-	# # Formatting Gallatin data to final data
-	# subjects = [] # TODO Build subjects list
-	# gallatin = {'longname': 'Gallatin','shortname': 'gallatin','subjects':subjects}
-	# data = {'schools':[gallatin]}
-	# return json.dumps(data)
+	gallatin = {'longname': 'Gallatin','shortname': 'gallatin','subjects':subject_list}
+	data = {'schools':[gallatin]}
+	return json.dumps(data)
+
+cols = ['COURSE', # ARTS-UG1275
+'CREDIT', # 4
+'DAYS', # Wed
+'DAYS2', # Fri
+'DESCRIPTION', # Text...
+'HISTCULT',
+'LIBARTS',
+'INSTRUCTORS', # [{'name':'website'}]
+'LEVEL', # U or G
+'NOTES', # Extra text
+'SECTION', # number
+'TERM', # FA, SU, WI, or SP
+'TIMES', # Time String
+'TIMES2', # Time String
+'TITLE', # Course Title
+'TYPE', # Arts Workshops (ARTS-UG)
+'YEAR'] # Integer
+
+match_global = re.compile('^Global')
+
+def should_include(row):
+	# Returns true if the row should be included in the final output
+	if match_global.match(row[index['type']]) is None:
+		return True
+	return False
+
+def append_subject(subjects,df):
+	pass
+
+def append_section(subjects, df):
+	# Adds the row data to the subjects dictionary
+	pass
+
+def append_period(subjects,df):
+	pass
+
+# Desired
+# {
+# 	"shortname": "ARTS-UG","longname": "Arts Workshops",
+# 	"listings": [
+# 			{
+# 				"shortname": "1485","longname": "FullCourseName","min_credits": 4,"max_credits": 4,
+# 				"description": "Course desc"
+# 				"sections": [
+# 					"shortname": "001",
+# 					"periods": [
+# 						{...}
+# 					]
+# 				]
+# 			}
+# 		]
+# 	},
+# 	{
+# 		"shortname": "IDSEM-UG","longname": "Interdisciplinary Seminars",
+# 		"listings": [
+# 			...
+# 		]
+# 	}
 
 def get_df(raw_json):
-	# Read in data, and drop the 'totalMatches' row
-	df = pd.read_json(raw_json, orient = 'index') # Read in data
-	df = df.drop('totalMatches',axis=0) # Drop 'totalMatches'
-	df.index = pd.to_numeric(df.index) # Reformat index
+# Read in data, and do some simple formatting
+	# Read in data
+	df = pd.read_json(raw_json, orient = 'index')
+	# For testing purposes only
+	df['description'] = df['description'].str[0:5]
+	# Drop useless columns
+	df = df.drop(['syllabus', 'term','year','level'],axis = 1)
+	# Drop  these two columns because they're annoying and IDK what to do with them
+	for name in ['foundation-histcult','foundation-libarts']:
+		if name in df.columns:
+			df = df.drop(name,axis = 1)
+	# Drop 'totalMatches' - it's not a record
+	df = df.drop('totalMatches',axis=0)
+	# To make it easier to work with, remove empty strings, None, and the like
+	df = df.replace(r'^\s*$',pd.np.nan,regex=True).fillna(value=pd.np.nan)
+
+# Filter out global courses
+	# Preformats a series to be used for filtering
+	type_col = df['type'].str.strip().str.lower()
+	# Filter out those global courses using a mask
+	df = df[type_col.str.startswith('global') == False]
+	del type_col # No longer need the series
+	# Reset index to integers (so that formatting doesn't screw up randomly)
+	df = df.reset_index(drop=True)
+
+# Format columns
+
+	# Replace 'type' with 'subject_longname'
+	# 'type' is formatted as :longname: (:shortname:-:level:)
+	type_parts = df['type'].str[:-1].str.split(r"\s*\(", n = 1, expand = True)
+	# Make the subject longname column
+	df['subject_longname'] = type_parts[0]
+	# Remove the 'type' column
+	df = df.drop('type',axis = 1)
+	del type_parts # we don't need this anymore
+
+	# Replace course with 'subject_shortname' and 'course_id'
+	# Change course into course shortname and course id
+	course_parts = df['course'].str.split(r'(?<=[A-Z])(?=[0-9])',n=1,expand=True)
+	df['subject_shortname'] = course_parts[0]
+	df['course_shortname'] = course_parts[1]
+
+
+	# making seperate first name column from new data frame
+	data["First Name"]= new[0]
+
+	# making seperate last name column from new data frame
+	data["Last Name"]= new[1]
+
+	# Dropping old Name columns
+	data.drop(columns =["Name"], inplace = True)
+
+
+
+
+
+	# Sort df so that we can directly append to list
+	# Group by type first, then within each type group by course name
+	df = df.sort_values(['type','course'])
+
 	return df
-
-# Below functions probably no longer useful
-def _shorten_desc(json_obj):
-	if 'course' in json_obj:
-		del json_obj['description']
-		json_obj['instructors'] = str(json_obj['instructors'])
-	return json_obj
-
-def load(json_data):
-	return json.loads(json_data)
-
-def to_list(raw_json):
-	del raw_json['totalMatches']
-	data = []
-	for key,value in raw_json.items():
-		data.append(value)
-	return data
-
-def filter_results(unfiltered_json):
-	# This will use exclusively the pandas package
-	# if this can be done more efficiently with json,
-	# it will use that instead.
-	return "filtered JSON"
