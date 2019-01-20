93e266cbb6a431257db02726302146d6114d9d01
Albert Liu <albertymliu@gmail.com>
2019-01-20 16:59:37 -0500

    Added `/metadata/` endpoints

--- pipelines/nyu/adapters/course_catalog/albert_client.rb
+++ pipelines/nyu/adapters/course_catalog/albert_client.rb
@@ -6,47 +6,91 @@ require 'pry'
 
 class AlbertClient
 
-	ALBERT_ROOT = 'https://m.albert.nyu.edu/app/catalog/classSearch'
+	ALBERT_ROOT = 'https://m.albert.nyu.edu/app/catalog/classSearch/'
 
 	def initialize
 		@http_client = HTTPClient.new
+		@csrf_token = get_csrf_token
 	end
 
-	# def listings_by_subject
+	def schools
+		get_drop_down_values 'search-acad-group'
+	end
 
-	# end
+	def terms
+		get_drop_down_values 'term'
+	end
 
-	# def listings
-	# 	url = "albert.com"
-	# 	xml = get_xml url
-	# end
-
-	def subjects
-		xml = get_xml ALBERT_ROOT
-		subject_nodes = xml.css('#search-acad-group').css('option')
-		subjects = subject_nodes[1..].map do |subject_node|
-			{
-				shortname: subject_node.attributes['value'].value,
-				longname: subject_node.text
-			}
-		end
+	def query_albert(term_shortname, school_shortname, subject_shortname)
+		response = post_request_xml(term_shortname, school_shortname, subject_shortname)
+		parse_response response
 	end
 
 	private
 
-	# def class_search
-	# 	body = {
-
-	# 	}
-	# end
-
-	def get_csrf_token
-		response = @http_client.get ALBERT_ROOT
-		response.headers['Set-Cookie'].match(/(?:[A-Za-z]+=)([^;]+)/).second
+	def post_request_xml(term_shortname, school_shortname, subject_shortname)
+		url = URI::join(ALBERT_ROOT,term_shortname)
+		body = {
+			CSRFToken: @csrf_token,
+			term: term_shortname,
+			acad_group: school_shortname,
+			subject: subject_shortname
+		}
+		post_xml(url, body)
 	end
 
-	def get_xml url
+	def parse_response response
+		response
+	end
+
+	def get_drop_down_values id
+		xml = get_xml ALBERT_ROOT
+		nodes = xml.css("##{id}").css('option')
+		nodes[1..].map do |node|
+			{
+				shortname: node.attributes['value'].value,
+				longname: node.text
+			}
+		end
+	end
+	def get_csrf_token
+		response = @http_client.get ALBERT_ROOT
+		response.headers['Set-Cookie'].match(/(?:[A-Za-z]+=)([^;]+)/)[1]
+	end
+	def post_xml(url, body)
+		response = @http_client.post(url, body)
+		binding.pry
+		Nokogiri::XML.parse(response.body)
+	end
+	def get_xml url # TODO take method name as string
+		# @http_client.send verb, params
 		response = @http_client.get(url)
 		Nokogiri::XML.parse(response.body)
+	end
+	def get_html url
+		response = @http_client.get(url)
+		Nokogiri::HTML.parse(response.body)
+	end
+	def subjects school_shortname# TODO Need to either run javascript or fill out form to continue
+		# Or lets just hard code this lol
+		[
+				['PH','Physics'],
+				['CM','Chemistry']
+		].map do |item|
+			{
+					shortname: item[0],
+					longname: item[1]
+			}
+		end
+		# body = get_xml ALBERT_ROOT
+		# subject_tag = body.css('#subject')
+		# binding.pry
+		# subject_nodes = subject_tag.css('option')
+		# subject_nodes.map do |subject_node|
+		# 	{
+		# 		shortname: subject_node.attributes['value'].value,
+		# 		longname: subject_node.text
+		# 	}
+		# end
 	end
 end
