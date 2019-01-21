aad4abbcb0be58e48fa49142d83c63405d4ef79e
Albert Liu <albertymliu@gmail.com>
2019-01-20 23:47:50 -0500

    commented out structure:

--- pipelines/nyu/adapters/course_catalog/src/process_data.rb
+++ pipelines/nyu/adapters/course_catalog/src/process_data.rb
@@ -4,10 +4,11 @@ require 'pry'
 def extract_listings html
 	elements = html.children
 	elements[0] = Hash.new { |h,k| h[k] = [] }
-	elements.inject do |first, second|
-		nil
-	end
-	html
+  binding.pry
+	# elements.inject do |first, second|
+	# 	nil
+	# end
+	nil
 end
 
 def parse_header div_tag
@@ -18,42 +19,42 @@ def parse_section a_tag
 
 end
 
-{
-    "schools": [
-        {
-            "longname": "Gallatin",
-            "shortname": "gallatin"
-            "subjects": [
-                {
-                    "shortname": "ARTS-UG",
-                    "longname": "Arts Workshops",
-                    "listings": [
-                      {
-                          "shortname": "1485",
-                          "longname": "Beyond Picture Perfect: Personal Choice in a Digital World",
-                          "min_credits": 4,
-                          "max_credits": 4,
-                          "description": "This course covers the very basic techniques of photography and digital imaging. Beyond Picture Perfect explores the many choices available to today\u2019s image makers. New technology combined with traditional photographic techniques will be addressed, enabling the students to realize their distinctive image-making vocabulary...",
-                          "crn":"",
-                          "sections": [
-                              shortname: "001",
-                              periods: [
-                                  {
-                                      ...
-                                  }
-                              ]
-                          ]
-                      }
-                    ]
-                },
-                {
-                    "shortname": "IDSEM-UG",
-                    "longname": "Interdisciplinary Seminars",
-                    "listings": [
-                        ...
-                    ]
-                }
-            ]
-        }
-    ]
-}
+# {
+#     "schools": [
+#         {
+#             "longname": "Gallatin",
+#             "shortname": "gallatin"
+#             "subjects": [
+#                 {
+#                     "shortname": "ARTS-UG",
+#                     "longname": "Arts Workshops",
+#                     "listings": [
+#                       {
+#                           "shortname": "1485",
+#                           "longname": "Beyond Picture Perfect: Personal Choice in a Digital World",
+#                           "min_credits": 4,
+#                           "max_credits": 4,
+#                           "description": "This course covers the very basic techniques of photography and digital imaging. Beyond Picture Perfect explores the many choices available to today\u2019s image makers. New technology combined with traditional photographic techniques will be addressed, enabling the students to realize their distinctive image-making vocabulary...",
+#                           "crn":"",
+#                           "sections": [
+#                               shortname: "001",
+#                               periods: [
+#                                   {
+#                                       ...
+#                                   }
+#                               ]
+#                           ]
+#                       }
+#                     ]
+#                 },
+#                 {
+#                     "shortname": "IDSEM-UG",
+#                     "longname": "Interdisciplinary Seminars",
+#                     "listings": [
+#                         ...
+#                     ]
+#                 }
+#             ]
+#         }
+#     ]
+# }
