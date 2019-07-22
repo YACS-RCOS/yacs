2327b88ded64200fc7c7eb10eedbe7d0e5d16f01
Malorn44 <Malorn44@gmail.com>
2019-07-22 14:42:20 -0400

    made the index parameter optional, defaul to 0

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -3,45 +3,52 @@ import { NoticeService } from './services/notice.service';
 import { SelectionService } from './services/selection.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [
-    NoticeService
-  ]
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	providers: [
+	NoticeService
+	]
 
 })
 export class AppComponent {
-  constructor(
-    private noticeService: NoticeService,
-    private selectionService: SelectionService) {}
+	constructor(
+		private noticeService: NoticeService,
+		private selectionService: SelectionService) {}
 
-  public showingSidebar: boolean = true;
-  
-  ngOnInit () {
-    this.showingSidebar = true;
+	public showingSidebar: boolean = true;
+	
+	ngOnInit () {
+		this.showingSidebar = true;
 
-    // check if schedule link entered
-    let url: string = window.location.href;
-    let i1: number = url.indexOf('schedules?section_ids=');
-    let i2: number = url.indexOf('&schedule_index=');
-    console.log(url);
-    if (i1 != -1 && i2 != -1) {
-      console.log(url.indexOf('schedules?section_ids='));
-      let sectionsStr = url.substring(i1 + 22, i2);
-      let sections: number[] = sectionsStr.split(',').map(Number);
-      console.log(sections);
-    }
-  }
+		// check if schedule link entered
+		let url: string = window.location.href;
+		let i1: number = url.indexOf('schedules?section_ids=');
+		let i2: number = url.indexOf('&schedule_index=');
+		console.log(url);
+		if (i1 != -1) {
+			console.log(url.indexOf('schedules?section_ids='));
+			let end: number = (i2 != -1) ? i2 : url.length;
+			let sections: number[] = url.substring(i1 + 22, end).split(',').map(Number);
+			console.log(sections);
+		
+			let index: number = 0;
+			if (i2 != -1) {
+				index = parseInt(url.substring(i2 + 16), 10);
+			}
 
-  public toggleSidebar (): void {
-    this.showingSidebar = !this.showingSidebar;
-  }
+			console.log(index);
+		}
+	}
 
-  //aids in moving contents of page down if notice bar is present
-  public hasNotice (): boolean {
-    return this.noticeService.hasNotice();
-  }
+	public toggleSidebar (): void {
+		this.showingSidebar = !this.showingSidebar;
+	}
+
+	//aids in moving contents of page down if notice bar is present
+	public hasNotice (): boolean {
+		return this.noticeService.hasNotice();
+	}
 
 
 }
