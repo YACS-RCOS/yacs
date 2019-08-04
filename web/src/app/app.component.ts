fbfcdb1e6c3af11512aea8474426c7033d80cb44
Malorn44 <Malorn44@gmail.com>
2019-08-04 01:03:10 -0400

    Removed commented code

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -23,47 +23,6 @@ export class AppComponent {
 	
 	ngOnInit () {
 		this.showingSidebar = true;
-
-		// // check if schedule link entered
-		// let url: string = window.location.href;
-		// let i1: number = url.indexOf('schedules?section_ids=');
-		// let i2: number = url.indexOf('&schedule_index=');
-		// console.log(url);
-		// if (i1 != -1) {
-		// 	console.log(url.indexOf('schedules?section_ids='));
-		// 	let end: number = (i2 != -1) ? i2 : url.length;
-		// 	const section_ids = url.substring(i1 + 22, end).split(',');//.map(Number);
-		// 	console.log(section_ids); // TODO: Remove
-		
-		// 	let index: number = 0;
-		// 	if (i2 != -1) {
-		// 		index = parseInt(url.substring(i2 + 16), 10);
-		// 	}
-
-		// 	console.log(index); // TODO: Remove
-			
-		// 	const store = this.selectionService.getSelectedSectionIds(true);
-		// 	// console.log(store);
-
-		// 	Section
-		// 	.where({id: section_ids})
-		// 	.includes('listing')
-		// 	.includes('listing.sections')
-		// 	.all().then((sections) => {
-		// 		// console.log(sections);
-		// 		sections.data.forEach(section => {
-		// 			console.log(section);
-		// 			this.selectionService.addSection(section);
-		// 		});
-		// 	});
-
-			// in scheduleView, there is a variable called scheduleSet
-			// that determines what schedules are displayed
-
-			// pass a parameter to the next event, it could be the sections
-			// modify getSelectedSectionIds to return the ones based on the url
-
-		// }
 	}
 
 	public toggleSidebar (): void {
