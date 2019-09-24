580d7693b7d7eb2707513355ec015dbe8a76c20d
Cole Gregory <gregoc3@rpi.edu>
2019-09-24 15:14:37 -0400

    Merge from master

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,31 +2,31 @@ import { Component, Input } from '@angular/core';
 import { NoticeService } from './services/notice.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [
-    NoticeService
-  ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [
+        NoticeService
+    ]
 
 })
 export class AppComponent {
-  constructor(private noticeService: NoticeService) {}
+    constructor(private noticeService: NoticeService) {}
 
-  public showingSidebar: boolean = true;
-  
-  ngOnInit () {
-    this.showingSidebar = true;
-  }
+    public showingSidebar: boolean = true;
 
-  public toggleSidebar (): void {
-    this.showingSidebar = !this.showingSidebar;
-  }
+    ngOnInit () {
+        this.showingSidebar = true;
+    }
 
-  //aids in moving contents of page down if notice bar is present
-  public hasNotice (): boolean {
-    return this.noticeService.hasNotice();
-  }
+    public toggleSidebar (): void {
+        this.showingSidebar = !this.showingSidebar;
+    }
+
+    // aids in moving contents of page down if notice bar is present
+    public hasNotice (): boolean {
+        return this.noticeService.hasNotice();
+    }
 
 
 }
