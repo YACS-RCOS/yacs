47943be45488385b110a5834b4d5b0cf0874b63e
marchdan <ackerd2@rpi.edu>
2019-10-01 16:17:30 -0400

    SIS man lives on

---
+++ web/src/app/no-results/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-results',
+  templateUrl: './component.html',
+  styleUrls: ['./component.scss']
+})
+
+export class NoResultsComponent {
+
+}
