4330cb852b41f73f35a526f27e22a1d9c3cfb8f8
Daniel Ackermans <ackerd2@rpi.edu>
2019-10-04 16:10:41 -0400

    Now it should be good

--- web/src/app/no-results/component.ts
+++ web/src/app/no-results/component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'no-results',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class NoResultsComponent implements OnInit {
+	
 	ngOnInit (){}
 }
